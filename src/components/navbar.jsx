8ca3563d18406a0d082695a65b2d234f866c83f2
Yoann <ybuzenet@gmail.com>
2020-01-29 20:59:46 +0100

    began navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = props => {
+  const isConnected = true;
+
+  const [toggle, setToggle] = useState(false);
+
   return (
     <>
-      <h1>Navbar</h1>
+      {toggle && (
+        <div className="unclick" onClick={() => setToggle(!toggle)}></div>
+      )}
+      <nav className="navbar">
+        <div className="container">
+          <p>Logo Boutique</p>
+          {(isConnected && (
+            <div className="my_options">
+              <p className="unselectable" onClick={() => setToggle(!toggle)}>
+                Prénom
+                <span
+                  className="arrow-menu unselectable"
+                  onClick={() => setToggle(!toggle)}
+                ></span>
+              </p>
+              {toggle && (
+                <ul className="toggle-menu">
+                  <li>Mon compte</li>
+                  <li>Mes rachats</li>
+                  <li>Déconnexion</li>
+                </ul>
+              )}
+            </div>
+          )) || (
+            <ul className="connect">
+              <li className="classic-links">S'inscrire</li>
+              <li className="classic-links">Se connecter</li>
+            </ul>
+          )}
+        </div>
+      </nav>
     </>
   );
 };
