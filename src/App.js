64f01831cc5c39fc2be68e2517426109e5c5bece
Yoann <ybuzenet@gmail.com>
2020-01-30 20:17:19 +0100

    setting up Authentication

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import "./App.css";
 import Navbar from "./components/navbar";
+import AuthContext from "./context/authContext";
+import AuthAPI from "./services/authAPI";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    AuthAPI.isAuthenticated()
+  );
+
+  const contextValue = {
+    isAuthenticated: isAuthenticated,
+    setIsAuthenticated: setIsAuthenticated
+  };
+
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <AuthContext.Provider value={contextValue}>
+      <div className="App">
+        <Navbar />
+      </div>
+    </AuthContext.Provider>
   );
 }
 
---
+++ src/services/authAPI.js
@@ -0,0 +1,58 @@
+import axios from "axios";
+import jwtDecode from "jwt-decode";
+
+function authenticate(credentials) {
+  return axios
+    .post("http://localhost:8000/api/login_check", credentials)
+    .then(response => response.data.token)
+    .then(token => {
+      //On stocke le token dans le local storage
+      window.localStorage.setItem("authToken", token);
+
+      //Faire en sorte maintenant que Axios porte le token dans son autorisation header de requete
+      //On affete ses parametres par defaut
+      axios.defaults.headers["Authorization"] = "Bearer " + token;
+
+      return true;
+    });
+}
+
+function logout() {
+  window.localStorage.removeItem("authToken");
+  delete axios.defaults.headers["Authorization"];
+}
+
+function setup() {
+  //1. Voir si on a un token
+  const token = window.localStorage.getItem("authToken");
+
+  //2. Verifier si le token est valide
+  if (token) {
+    const jwtData = jwtDecode(token);
+
+    if (jwtData.exp * 1000 > new Date().getTime()) {
+      //3. Donner le token à axios
+      axios.defaults.headers["Authorization"] = "Bearer " + token;
+    }
+  }
+}
+
+function isAuthenticated() {
+  //1. Voir si on a un token
+  const token = window.localStorage.getItem("authToken");
+  //Vérifier si la date d'exp du token est bonne, si oui on considère que connecté ( le serveur fait le vrai check derrière)
+  if (token) {
+    const jwtData = jwtDecode(token);
+
+    return jwtData.exp * 1000 > new Date().getTime();
+  } else {
+    return false;
+  }
+}
+
+export default {
+  authenticate: authenticate,
+  logout: logout,
+  setup: setup,
+  isAuthenticated: isAuthenticated
+};
