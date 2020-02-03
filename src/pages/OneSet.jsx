95b4f61e200cbe73c73829bb8198788c81555a83
Yoann <ybuzenet@gmail.com>
2020-02-03 10:03:57 +0100

    function to add to elling basket works with double cards

--- src/pages/OneSet.jsx
+++ src/pages/OneSet.jsx
@@ -10,6 +10,41 @@ const OneSet = props => {
     console.log(currentBasket);
   });
 
+  const handleAddSellingBasket = (currentBasket, card) => {
+    for (var i = 0; i < currentBasket.length; i++) {
+      if (
+        currentBasket[i].cardName === card.cardName &&
+        currentBasket[i].set === card.set &&
+        currentBasket[i].price === card.price &&
+        currentBasket[i].condition === card.condition &&
+        currentBasket[i].lang === card.lang &&
+        currentBasket[i].isFoil === card.isFoil &&
+        currentBasket[i].uuid === card.uuid
+      ) {
+        const updatedCard = currentBasket[i];
+        updatedCard.quantity += card.quantity;
+
+        // setCurrentbasket by adding quantity of the card currently added to the selling basket
+        setCurrentBasket(
+          currentBasket.map(card => {
+            return card.cardName === updatedCard.cardName &&
+              card.set === updatedCard.set &&
+              card.price === updatedCard.price &&
+              card.condition === updatedCard.condition &&
+              card.lang === updatedCard.lang &&
+              card.isFoil === updatedCard.isFoil &&
+              card.uuid === updatedCard.uuid
+              ? { ...updatedCard }
+              : card;
+          })
+        );
+        break;
+      } else {
+        setCurrentBasket([...currentBasket, card]);
+      }
+    }
+  };
+
   return (
     <>
       <div className="container">
@@ -22,7 +57,7 @@ const OneSet = props => {
                 <thead></thead>
                 <tbody>
                   <tr>
-                    <td>NOM CARTE</td>
+                    <td>Dague de vif-argent</td>
                     <td>LANGUE</td>
                     <td>
                       <select name="" id="">
@@ -62,20 +97,17 @@ const OneSet = props => {
                       <i
                         className="fas fa-plus-circle add-item-basket"
                         onClick={() =>
-                          setCurrentBasket([
-                            ...currentBasket,
-                            {
-                              cardName: "Dague de vif-argent",
-                              set: "Invasion",
-                              price: 2,
-                              condition: "NM",
-                              lang: "EN",
-                              isFoil: "No",
-                              uuid: "9215-ddfsdf-9898-dsfdc",
-                              currency: "euros",
-                              quantity: 4
-                            }
-                          ])
+                          handleAddSellingBasket(currentBasket, {
+                            cardName: "Dague de vif-argent",
+                            set: "Invasion",
+                            price: 2,
+                            condition: "NM",
+                            lang: "EN",
+                            isFoil: "No",
+                            uuid: "9215-ddfsdf-9898-dsfdc",
+                            currency: "euros",
+                            quantity: 4
+                          })
                         }
                       ></i>
                     </td>
