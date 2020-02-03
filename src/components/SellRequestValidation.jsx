6d97e47dac73b87b6e3930c8eebf37c3dcbd293c
Yoann <ybuzenet@gmail.com>
2020-02-03 12:51:17 +0100

    adding check on Sell Request submission

---
+++ src/components/SellRequestValidation.jsx
@@ -0,0 +1,49 @@
+import React, { useContext } from "react";
+import SellingBasketContext from "../context/sellingBasket";
+import AuthContext from "../context/authContext";
+
+const SellRequestValidation = props => {
+  //Current Basket
+  const { currentBasket, setCurrentBasket } = useContext(SellingBasketContext);
+
+  //Current Authentication
+  const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="sellRequest-validation-block">
+      <h2>Valider mon rachat</h2>
+      {/* Div is changing if the user has an empty basket or not */}
+      {(currentBasket.length > 0 && (
+        <div className="isCurrentBasketReady">
+          {isAuthenticated ? (
+            <div className="isUserAuthenticated">
+              <form action="" onSubmit={handleSubmit}>
+                <input type="checkbox" required />
+                <span>J'accepte les conditions générales de ventes.</span>
+                <button className="sellRequest-validation-button" type="submit">
+                  Valider mon rachat
+                </button>
+              </form>
+            </div>
+          ) : (
+            <div className="userShouldConnect">
+              Pour soumettre complètement votre rachat, vous devez vous inscrire
+              ou vous connecter.<p>(Votre panier ne sera pas perdu)</p>
+            </div>
+          )}
+        </div>
+      )) || (
+        <div className="isCurrentBasketEmpty">
+          Votre panier est vide. Pour le valider, merci d'ajouter des cartes à
+          votre rachat.
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SellRequestValidation;
--- src/pages/MySellingBasket.jsx
+++ src/pages/MySellingBasket.jsx
@@ -1,10 +1,9 @@
 import React, { useContext } from "react";
 import SellingBasketContext from "../context/sellingBasket";
+import SellRequestValidation from "../components/SellRequestValidation";
 
 const MyCurrentSellRequest = props => {
   const { currentBasket, setCurrentBasket } = useContext(SellingBasketContext);
-
-  console.log(currentBasket);
 
   const handleDelete = cardUuid => {
     const newCurrentBasket = currentBasket.filter(
@@ -29,84 +28,92 @@ const MyCurrentSellRequest = props => {
 
   return (
     <>
-      <div className="container">
-        <h1>Ma demande de rachat</h1>
-        <table className="zebra-table">
-          <thead>
-            <tr>
-              <th>Nom de la carte</th>
-              <th>Edition</th>
-              <th>Etat</th>
-              <th>Langue</th>
-              <th>Foil</th>
-              <th>Prix</th>
-              <th>Quantité</th>
-              <th>Total</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {currentBasket.map((card, index) => (
-              <tr key={index}>
-                <td>{card.cardName}</td>
-                <td>{card.set}</td>
-                <td>{card.condition}</td>
-                <td>{card.lang}</td>
-                <td>{card.isFoil}</td>
-                <td>{card.price}</td>
+      <div className="all-content sell-request">
+        <div className="left-content"></div>
+        <div className="main-content">
+          <h1>Ma demande de rachat</h1>
+          <table className="zebra-table">
+            <thead>
+              <tr>
+                <th>Nom de la carte</th>
+                <th>Edition</th>
+                <th>Etat</th>
+                <th>Langue</th>
+                <th>Foil</th>
+                <th>Prix</th>
+                <th>Quantité</th>
+                <th>Total</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {currentBasket.map((card, index) => (
+                <tr key={index}>
+                  <td>{card.cardName}</td>
+                  <td>{card.set}</td>
+                  <td>{card.condition}</td>
+                  <td>{card.lang}</td>
+                  <td>{card.isFoil}</td>
+                  <td>{card.price}</td>
+                  <td>
+                    <select
+                      name=""
+                      id=""
+                      onChange={event =>
+                        handleChange(event, currentBasket, card)
+                      }
+                    >
+                      <option value={card.quantity}>{card.quantity}</option>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                      <option value="7">7</option>
+                      <option value="8">8</option>
+                      <option value="9">9</option>
+                      <option value="10">10</option>
+                      <option value="11">11</option>
+                      <option value="12">12</option>
+                    </select>
+                  </td>
+                  <td>{card.price * card.quantity}</td>
+                  {total_prices.push(card.price * card.quantity)}
+                  <td>
+                    <i
+                      className="fas fa-minus-circle delete-from-selling-basket"
+                      onClick={() => handleDelete(card.uuid)}
+                    ></i>
+                  </td>
+                </tr>
+              ))}
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
                 <td>
-                  <select
-                    name=""
-                    id=""
-                    onChange={event => handleChange(event, currentBasket, card)}
-                  >
-                    <option value={card.quantity}>{card.quantity}</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                    <option value="10">10</option>
-                    <option value="11">11</option>
-                    <option value="12">12</option>
-                  </select>
+                  Total cartes :
+                  {currentBasket.reduce((total, card) => {
+                    return total + card.quantity;
+                  }, 0)}
                 </td>
-                <td>{card.price * card.quantity}</td>
-                {total_prices.push(card.price * card.quantity)}
                 <td>
-                  <i
-                    className="fas fa-minus-circle delete-from-selling-basket"
-                    onClick={() => handleDelete(card.uuid)}
-                  ></i>
+                  Total :
+                  {total_prices.reduce((total, number) => {
+                    return total + number;
+                  }, 0)}
                 </td>
               </tr>
-            ))}
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td>
-                Total cartes :
-                {currentBasket.reduce((total, card) => {
-                  return total + card.quantity;
-                }, 0)}
-              </td>
-              <td>
-                Total :
-                {total_prices.reduce((total, number) => {
-                  return total + number;
-                }, 0)}
-              </td>
-            </tr>
-          </tbody>
-        </table>
+            </tbody>
+          </table>
+        </div>
+        <div className="right-content">
+          <SellRequestValidation />
+        </div>
       </div>
     </>
   );
