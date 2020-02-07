1e3b516769be29d8d0a3c78a3bb342276ea1443a
Yoann <ybuzenet@gmail.com>
2020-02-07 13:45:33 +0100

    onHover card is working

--- src/components/CardLine.jsx
+++ src/components/CardLine.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState, useEffect } from "react";
 import SellingBasketContext from "../context/sellingBasket";
 
-const CardLine = ({ card, handleAddSellingBasket }) => {
+const CardLine = ({ card, handleAddSellingBasket, index }) => {
   //Current Selling Request Basket
   const { currentBasket, setCurrentBasket } = useContext(SellingBasketContext);
 
@@ -32,7 +32,7 @@ const CardLine = ({ card, handleAddSellingBasket }) => {
   const firstCharac = card.picture.substr(0, 1);
   const secondCharac = card.picture.substr(1, 1);
   const urlCard =
-    "aaahttps://img.scryfall.com/cards/small/front/" +
+    "https://img.scryfall.com/cards/small/front/" +
     firstCharac +
     "/" +
     secondCharac +
@@ -42,7 +42,8 @@ const CardLine = ({ card, handleAddSellingBasket }) => {
 
   return (
     <>
-      <td
+      <tr
+        key={index}
         onMouseEnter={() => {
           setIsOnHover(!isOnHover);
         }}
@@ -50,83 +51,86 @@ const CardLine = ({ card, handleAddSellingBasket }) => {
           setIsOnHover(!isOnHover);
         }}
       >
-        {card.cardName}
-        {isOnHover && (
-          <div className="cardPicture">
-            <img src={urlCard} alt={card.name} />
-          </div>
-        )}
-      </td>
-      <td>
-        {/* Select will have to be refactored with a .map on a Select Component */}
-        <select
-          name="lang"
-          id={card.cardName + "id1"}
-          onChange={event => {
-            handleChange(event, currentCard);
-          }}
-        >
-          <option value={card.lang}>{card.lang}</option>
-          <option value="FR">FR</option>
-          <option value="ES">ES</option>
-        </select>
-      </td>
-      <td>
-        <select
-          name="condition"
-          id={card.cardName + "id2"}
-          onChange={event => {
-            handleChange(event, currentCard);
-          }}
-        >
-          <option value={card.condition}>{card.condition}</option>
-          <option value="EXC">EXC</option>
-          <option value="PL">PL</option>
-        </select>
-      </td>
-      <td>
-        <select
-          name="quantity"
-          id={card.cardName + "id3"}
-          onChange={event => {
-            handleChange(event, currentCard);
-          }}
-        >
-          <option value={card.quantity}>{card.quantity}</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-          <option value="11">11</option>
-          <option value="12">12</option>
-        </select>
-      </td>
-      <td>
-        <select
-          name="isFoil"
-          id={card.cardName + "id4"}
-          onChange={event => {
-            handleChange(event, currentCard);
-          }}
-        >
-          <option value={card.isFoil}>{card.isFoil}</option>
-          <option value="Yes">Yes</option>
-          <option value="No">No</option>
-        </select>
-      </td>
-      <td>{card.price}</td>
-      <td>
-        <i
-          className="fas fa-plus-circle add-item-basket"
-          onClick={() => handleAddSellingBasket(currentBasket, currentCard)}
-        ></i>
-      </td>
+        <td className="cardPictureHolder">
+          {card.cardName}
+          {isOnHover && (
+            //TODO : change className following the scrolling, to know if the position must be top or bottom, to stay in window
+            <div className="cardPictureOnHover">
+              <img src={urlCard} alt={card.name} />
+            </div>
+          )}
+        </td>
+        <td>
+          {/* Select will have to be refactored with a .map on a Select Component */}
+          <select
+            name="lang"
+            id={card.cardName + "id1"}
+            onChange={event => {
+              handleChange(event, currentCard);
+            }}
+          >
+            <option value={card.lang}>{card.lang}</option>
+            <option value="FR">FR</option>
+            <option value="ES">ES</option>
+          </select>
+        </td>
+        <td>
+          <select
+            name="condition"
+            id={card.cardName + "id2"}
+            onChange={event => {
+              handleChange(event, currentCard);
+            }}
+          >
+            <option value={card.condition}>{card.condition}</option>
+            <option value="EXC">EXC</option>
+            <option value="PL">PL</option>
+          </select>
+        </td>
+        <td>
+          <select
+            name="quantity"
+            id={card.cardName + "id3"}
+            onChange={event => {
+              handleChange(event, currentCard);
+            }}
+          >
+            <option value={card.quantity}>{card.quantity}</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+          </select>
+        </td>
+        <td>
+          <select
+            name="isFoil"
+            id={card.cardName + "id4"}
+            onChange={event => {
+              handleChange(event, currentCard);
+            }}
+          >
+            <option value={card.isFoil}>{card.isFoil}</option>
+            <option value="Yes">Yes</option>
+            <option value="No">No</option>
+          </select>
+        </td>
+        <td>{card.price}</td>
+        <td>
+          <i
+            className="fas fa-plus-circle add-item-basket"
+            onClick={() => handleAddSellingBasket(currentBasket, currentCard)}
+          ></i>
+        </td>
+      </tr>
     </>
   );
 };
