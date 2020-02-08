2895bf07f4c6df829bd2972700e1da8be243cf7f
Yoann <ybuzenet@gmail.com>
2020-02-08 11:22:38 +0100

    languages et conditions now OK, LOCALLY managed

--- src/components/LastModifications.jsx
+++ src/components/LastModifications.jsx
@@ -15,7 +15,7 @@ const LastModifications = ({ handleAddSellingBasket }) => {
             picture="https://placehold.it/100x175"
             handleAddSellingBasket={handleAddSellingBasket}
             card={{
-              cardName: "Foret de la Yavimaya",
+              name: "Foret de la Yavimaya",
               set: "Invasion",
               price: 2,
               condition: "NM",
@@ -23,14 +23,15 @@ const LastModifications = ({ handleAddSellingBasket }) => {
               isFoil: "No",
               uuid: "9215-ddfsdf-9898-dsfde",
               currency: "euros",
-              quantity: 1
+              quantity: 1,
+              scryfallid: "d3c99f65-2355-444b-b49a-c6b916f268b1"
             }}
           />
           <CardWithThumbnail
             picture="https://placehold.it/100x175"
             handleAddSellingBasket={handleAddSellingBasket}
             card={{
-              cardName: "Kavru Languefeu",
+              name: "Kavru Languefeu",
               set: "Planeshift",
               price: 2,
               condition: "EXC",
@@ -38,14 +39,15 @@ const LastModifications = ({ handleAddSellingBasket }) => {
               isFoil: "No",
               uuid: "9215-ddfsdf-9898-dsfd",
               currency: "euros",
-              quantity: 1
+              quantity: 1,
+              scryfallid: "d3c99f65-2355-444b-b49a-c6b916f268b1"
             }}
           />
           <CardWithThumbnail
             picture="https://placehold.it/100x175"
             handleAddSellingBasket={handleAddSellingBasket}
             card={{
-              cardName: "Cape de tatou",
+              name: "Cape de tatou",
               set: "Invasion",
               price: 2,
               condition: "PL",
@@ -53,14 +55,15 @@ const LastModifications = ({ handleAddSellingBasket }) => {
               isFoil: "No",
               uuid: "9215-ddfsdf-9898-dsfdi",
               currency: "euros",
-              quantity: 1
+              quantity: 1,
+              scryfallid: "d3c99f65-2355-444b-b49a-c6b916f268b1"
             }}
           />
           <CardWithThumbnail
             picture="https://placehold.it/100x175"
             handleAddSellingBasket={handleAddSellingBasket}
             card={{
-              cardName: "Apprenti Spinosophe",
+              name: "Apprenti Spinosophe",
               set: "Invasion",
               price: 2,
               condition: "NM",
@@ -68,7 +71,8 @@ const LastModifications = ({ handleAddSellingBasket }) => {
               isFoil: "Yes",
               uuid: "9215-ddfsdf-9898-dsfdn",
               currency: "euros",
-              quantity: 1
+              quantity: 1,
+              scryfallid: "d3c99f65-2355-444b-b49a-c6b916f268b1"
             }}
           />
         </div>
---
+++ src/definitions/languagesDefinition.js
@@ -0,0 +1,61 @@
+const allLanguages = [
+  {
+    id: 1,
+    name: "German",
+    shortname: "DE"
+  },
+  {
+    id: 2,
+    name: "Spanish",
+    shortname: "SP"
+  },
+  {
+    id: 3,
+    name: "French",
+    shortname: "FR"
+  },
+  {
+    id: 4,
+    name: "Italian",
+    shortname: "IT"
+  },
+  {
+    id: 5,
+    name: "Japanese",
+    shortname: "JP"
+  },
+  {
+    id: 6,
+    name: "Portuguese (Brazil)",
+    shortname: "PT"
+  },
+  {
+    id: 7,
+    name: "Russian",
+    shortname: "RU"
+  },
+  {
+    id: 8,
+    name: "Chinese Simplified",
+    shortname: "CHS"
+  },
+  {
+    id: 9,
+    name: "English",
+    shortname: "EN"
+  },
+  {
+    id: 10,
+    name: "Korean",
+    shortname: "KO"
+  },
+  {
+    id: 11,
+    name: "Chinese Traditional",
+    shortname: "CHT"
+  }
+];
+
+export default {
+  allLanguages
+};
