3969c1f7226c44d2a542508e497e699796a277b9
Yoann <ybuzenet@gmail.com>
2020-02-10 18:32:50 +0100

    refactoring of the Sell Request Submission

--- src/components/SellRequestValidation.jsx
+++ src/components/SellRequestValidation.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import SellingBasketContext from "../context/sellingBasket";
 import AuthContext from "../context/authContext";
 import canSubmitContext from "../context/canSubmitSellRequestContext";
+import ValidSellRequestIsBasketEmpty from "./ValidSellRequestIsBasketEmpty";
 
 const SellRequestValidation = ({ history }) => {
   //Current Basket
@@ -82,35 +83,7 @@ const SellRequestValidation = ({ history }) => {
   return (
     <div className="sellRequest-validation-block">
       <h2>Valider mon rachat</h2>
-      {/* Div is changing if the user has an empty basket or not */}
-      {(currentBasket.length > 0 && (
-        <div className="isCurrentBasketReady">
-          {authenticationInfos.isAuthenticated ? (
-            <div className="isUserAuthenticated">
-              <form action="" onSubmit={handleSubmit}>
-                <input type="checkbox" required id="checkbox-compliance" />
-                <label htmlFor="checkbox-compliance">
-                  J'accepte les conditions générales de ventes.
-                </label>
-                <button className="sellRequest-validation-button" type="submit">
-                  Valider mon rachat
-                </button>
-              </form>
-            </div>
-          ) : (
-            <div className="userShouldConnect">
-              Pour soumettre complètement votre rachat, vous devez vous inscrire
-              ou vous connecter.
-              <p>(Votre panier ne sera pas perdu)</p>
-            </div>
-          )}
-        </div>
-      )) || (
-        <div className="isCurrentBasketEmpty">
-          Votre panier est vide. Pour le valider, merci d'ajouter des cartes à
-          votre rachat.
-        </div>
-      )}
+      <ValidSellRequestIsBasketEmpty handleSubmit={handleSubmit} />
     </div>
   );
 };
