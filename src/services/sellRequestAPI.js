13f64cb05ce14911604f5b77e73101d0dc22b9c9
Yoann <ybuzenet@gmail.com>
2020-02-12 18:13:55 +0100

    cleanup effect added on api call

--- src/services/sellRequestAPI.js
+++ src/services/sellRequestAPI.js
@@ -8,9 +8,9 @@ function findAll() {
   return axios.get("http://127.0.0.1:8000/sell_requests");
 }
 
-function findById(id) {
+function findById(id, cleanUpParam) {
   return axios
-    .get("http://127.0.0.1:8000/sell_requests/" + id)
+    .get("http://127.0.0.1:8000/sell_requests/" + id, cleanUpParam)
     .then(response => response.data);
 }
 
