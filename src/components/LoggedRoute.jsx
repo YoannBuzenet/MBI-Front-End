af5eb1f5ee994e91ba792c68eee503d4a4542fba
Yoann <ybuzenet@gmail.com>
2020-02-12 17:17:34 +0100

    working on On Sell Request Page

--- src/components/LoggedRoute.jsx
+++ src/components/LoggedRoute.jsx
@@ -1,11 +1,15 @@
 import React, { useContext } from "react";
 import { Redirect, Route } from "react-router-dom";
 import AuthContext from "../context/authContext";
+import { withRouter } from "react-router-dom";
 
-const LoggedRoute = ({ path, component }) => {
+const LoggedRoute = ({ path, component, match, history }) => {
   const { authenticationInfos, setAuthenticationInfos } = useContext(
     AuthContext
   );
+
+  component = withRouter(component);
+
   return authenticationInfos.isAuthenticated ? (
     <Route path={path} component={component} />
   ) : (
