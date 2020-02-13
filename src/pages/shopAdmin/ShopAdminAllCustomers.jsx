e75c446e77dac70d9a2ae7efd2fd151f083bf5a2
Yoann <ybuzenet@gmail.com>
2020-02-13 12:56:52 +0100

    all customers page working

--- src/pages/shopAdmin/ShopAdminAllCustomers.jsx
+++ src/pages/shopAdmin/ShopAdminAllCustomers.jsx
@@ -5,15 +5,48 @@ import axios from "axios";
 const ShopAdminAllCustomers = props => {
   const [listCustomers, setListCustomers] = useState([]);
 
+  //Preparing the variable that allow Promises clean up on Axios
+  const CancelToken = axios.CancelToken;
+  const source = CancelToken.source();
+
   useEffect(() => {
-    console.log(axios.defaults.headers);
-    customersAPI.findAll().then(response => setListCustomers(response));
+    customersAPI
+      .findAll({
+        cancelToken: source.token
+      })
+      .then(response => setListCustomers(response));
     console.log("hey");
+
+    return () => source.cancel("");
   }, []);
   return (
     <>
       <h1>Customers</h1>
-      {listCustomers.length > 0 && listCustomers.map(customer => "lol")}
+      <table className="zebra-table">
+        <thead>
+          <tr>
+            <th>Reference</th>
+            <th>Nom</th>
+            <th>Prénom</th>
+            <th>Nombre de Rachats</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listCustomers.length > 0 &&
+            listCustomers.map(customer => {
+              console.log(customer);
+
+              return (
+                <tr key={customer.id}>
+                  <td>{customer.id}</td>
+                  <td>{customer.nom}</td>
+                  <td>{customer.prenom}</td>
+                  <td>{customer.sellrequest.length}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
     </>
   );
 };
