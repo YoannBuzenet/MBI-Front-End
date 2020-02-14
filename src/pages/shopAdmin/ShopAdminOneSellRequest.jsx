938131eafb303dc351a89e2c34507efbb5003a70
Yoann <ybuzenet@gmail.com>
2020-02-14 11:28:20 +0100

    began Sell Request Edition by Shop

--- src/pages/shopAdmin/ShopAdminOneSellRequest.jsx
+++ src/pages/shopAdmin/ShopAdminOneSellRequest.jsx
@@ -1,12 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import sellRequestAPI from "../../services/sellRequestAPI";
+import StatusCalculator from "../../components/StatusCalculator";
+import LastInformationCalculator from "../../components/LastInformationCalculator";
+import CardLineShop from "../../components/shop/CardLineShop";
 
 const ShopAdminOneSellRequest = ({ match }) => {
   const { id } = match.params;
 
-  console.log(id);
+  const [currentSellRequest, setCurrentSellRequest] = useState({
+    sellRequestCards: []
+  });
+
+  useEffect(() => {
+    const CancelToken = axios.CancelToken;
+    const source = CancelToken.source();
+
+    sellRequestAPI
+      .findById(id, {
+        cancelToken: source.token
+      })
+      .then(data => {
+        setCurrentSellRequest(data);
+      });
+
+    return () => source.cancel("");
+  }, [id]);
+
+  //ENV VARIABLE TO DEFINE
+  const gradingArea = "isEU";
+
   return (
     <>
       <h1>One Sell Request</h1>
+      <div className="sellRequest-infos">
+        <p className="sellRequest-status">
+          Statut
+          <span className="subInfos">
+            <StatusCalculator sellRequest={currentSellRequest} />
+          </span>
+        </p>
+        <p className="sellRequest-lastDate">
+          Dernière information
+          <span className="subInfos">
+            <LastInformationCalculator sellRequest={currentSellRequest} />
+          </span>
+        </p>
+      </div>
+      <table className="zebra-table">
+        <thead>
+          <tr>
+            <th>Nom de la carte</th>
+            <th>Edition</th>
+            <th>Etat</th>
+            <th>Langue</th>
+            <th>Foil</th>
+            <th>Prix</th>
+            <th>Quantité</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentSellRequest.sellRequestCards.length > 0 &&
+            currentSellRequest.sellRequestCards.map((card, index) => (
+              <tr key={index}>
+                <CardLineShop card={card} />
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </>
   );
 };
