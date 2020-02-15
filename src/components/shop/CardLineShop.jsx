03ba7f447d7be02b5aa3bc106c5c1ccac98811bd
Yoann <ybuzenet@gmail.com>
2020-02-15 10:51:00 +0100

    Shop One Sell request less buggy + cleaned up code

--- src/components/shop/CardLineShop.jsx
+++ src/components/shop/CardLineShop.jsx
@@ -1,8 +1,5 @@
 import React, { useContext, useState, useEffect } from "react";
-import SellingBasketContext from "../../context/sellingBasket";
-import SellingBasketAPI from "../../services/sellingBasketAPI";
 import GenericCardInfosContext from "../../context/genericCardInfosContext";
-import sellingBasketAPI from "../../services/sellingBasketAPI";
 import canSubmitContext from "../../context/canSubmitSellRequestContext";
 import genericCardAPI from "../../services/genericCardAPI";
 
@@ -21,7 +18,18 @@ const CardLineShop = ({
   const { lang, conditions } = useContext(GenericCardInfosContext);
 
   //Using the current Card in state
-  const [currentCard, setCard] = useState(card);
+  const [currentCard, setCurrentCard] = useState({
+    name: card.cards.name,
+    scryfallid: card.cards.scryfallid,
+    hasfoil: 1,
+    hasnonfoil: 1,
+    uuid: card.cards.uuid,
+    foreignData: card.cards.foreignData,
+    condition: card.CardCondition.id,
+    lang: card.language.id,
+    set: card.cards.edition.name,
+    quantity: card.cardQuantity
+  });
 
   //Saving the Hover state
   const [isOnHover, setIsOnHover] = useState(false);
@@ -41,6 +49,69 @@ const CardLineShop = ({
       console.log(conditions);
     }
   }, [isOnHover]);
+
+  //We transform what we got from the API into code that is compatible with the App
+  useEffect(() => {
+    setCurrentCard({
+      name: card.cards.name,
+      scryfallid: card.cards.scryfallid,
+      hasfoil: 1,
+      hasnonfoil: 1,
+      uuid: card.cards.uuid,
+      foreignData: card.cards.foreignData,
+      condition: card.CardCondition.id,
+      lang: card.language.id,
+      set: card.cards.edition.name,
+      quantity: card.cardQuantity
+    });
+    //      ICI on doit state la current card, transformer card en current card pour que ça match
+    //      CE qu'on veut
+    // @id: "/cards/5879"
+    // @type: "Cards"
+    // hasfoil: 1
+    // hasnonfoil: 1
+    // name: "Alchemist's Refuge"
+    // scryfallid: "c767a897-52e3-4401-8104-930157bb2b02"
+    // uuid: "eacc9092-28b1-5286-8636-59a63f4296d5"
+    // foreignData: (10) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+    // quantity: 1
+    // condition: "2"
+    // lang: "9"
+    // isFoil: "No"
+    // set: "Avacyn Restored"
+    // price: 1
+    //     Ce qu'on a
+    //     @id: "/sell_request_cards/53"
+    // @type: "SellRequestCard"
+    // id: 53
+    // price: 1
+    // dateValidated: null
+    // language:
+    // @id: "/languages/1"
+    // @type: "Language"
+    // id: 1
+    // name: "German"
+    // shortname: "DE"
+    // __proto__: Object
+    // CardCondition:
+    // @id: "/card_conditions/2"
+    // @type: "CardCondition"
+    // id: 2
+    // shortname: "NM"
+    // shortnameUS: "NM"
+    // __proto__: Object
+    // cards:
+    // @id: "/cards/5168"
+    // @type: "Cards"
+    // name: "Ceta Disciple"
+    // scryfallid: "b1c40c26-3b82-4f72-acb5-85fbdd51665a"
+    // uuid: "a85a7827-3597-5138-b625-9f574ed45c96"
+    // edition: {@id: "/sets/19", @type: "Sets", id: 19, name: "Apocalypse"}
+    // foreignData: (6) [{…}, {…}, {…}, {…}, {…}, {…}]
+    // __proto__: Object
+    // cardQuantity: 7
+    // isFoil: false
+  }, [card]);
 
   useEffect(() => {
     if (isLoaded) {
@@ -68,7 +139,7 @@ const CardLineShop = ({
 
     setIsloaded(true);
     setErrorList([]);
-    setCard({ ...currentCard, [name]: newValue });
+    setCurrentCard({ ...currentCard, [name]: newValue });
     console.log(currentCard);
   };
 
@@ -84,15 +155,15 @@ const CardLineShop = ({
   };
 
   //Creating the specific link to get the scryffalID picture. It is composed of a static base, + the 2 first character of the ID, + the ID
-  const firstCharac = card.cards.scryfallid.substr(0, 1);
-  const secondCharac = card.cards.scryfallid.substr(1, 1);
+  const firstCharac = currentCard.scryfallid.substr(0, 1);
+  const secondCharac = currentCard.scryfallid.substr(1, 1);
   const urlCard =
     "https://img.scryfall.com/cards/small/front/" +
     firstCharac +
     "/" +
     secondCharac +
     "/" +
-    card.cards.scryfallid +
+    currentCard.scryfallid +
     ".jpg";
 
   //Defining the class, if it's error or not
@@ -106,7 +177,6 @@ const CardLineShop = ({
   return (
     <>
       <tr
-        key={parseInt(card["@id"].substr(7))}
         onMouseEnter={e => {
           setIsOnHover(!isOnHover);
           setHoverTopOrBottom(hoverClassName(e));
@@ -147,28 +217,22 @@ const CardLineShop = ({
                           parseInt(card.language.id)
                       )[0].language_id.shortname}
                 </option>
-              ]
-                .concat(
-                  card.cards.foreignData
-                    .filter(
-                      currentlanguage =>
-                        currentlanguage.language_id.id !==
-                        parseInt(card.language.id)
-                    )
-                    .map((foreignData, index) => (
-                      <option
-                        value={foreignData.language_id.id}
-                        key={index + "3"}
-                      >
-                        {foreignData.language_id.shortname}
-                      </option>
-                    ))
-                )
-                .concat([
-                  <option value="9" key={card.id + "3" + card.language.id}>
-                    EN
-                  </option>
-                ])
+              ].concat(
+                card.cards.foreignData
+                  .filter(
+                    currentlanguage =>
+                      currentlanguage.language_id.id !==
+                      parseInt(card.language.id)
+                  )
+                  .map((foreignData, index) => (
+                    <option
+                      value={foreignData.language_id.id}
+                      key={index + "3"}
+                    >
+                      {foreignData.language_id.shortname}
+                    </option>
+                  ))
+              )
             ) : (
               <option value="9">EN</option>
             )}
