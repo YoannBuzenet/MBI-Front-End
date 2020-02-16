bd5e2dbb50b4d9cc9df93544f38f6007d4a69521
Yoann <ybuzenet@gmail.com>
2020-02-16 21:37:22 +0100

    implementing modal

--- src/components/EditionChoosingModal.jsx
+++ src/components/EditionChoosingModal.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 
-const EditionChoosingModal = props => {
-  console.log("je suis là");
+const EditionChoosingModal = ({ editionInformations, currentCard }) => {
+  console.log(editionInformations);
   return (
     <>
+      {/* TODO METTRE UN ONCLICK DISABLE PARENT STATE ON MODAL */}
       <div className="blackSheet"></div>
-      <div className="edition-modal">Modal</div>
+      <div className="edition-modal">
+        CHANGER L'EDITION
+        <select value={currentCard.set}>
+          {editionInformations &&
+            editionInformations.map(card => (
+              <option>{card.edition.name}</option>
+            ))}
+        </select>
+      </div>
     </>
   );
 };
