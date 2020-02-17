2ce78d99df60f8eefb1d8220ca44606fcc88dc5b
Yoann <ybuzenet@gmail.com>
2020-02-17 20:35:04 +0100

    completed STUCK component cards in validated sell request

--- src/components/shop/CardLineShopStuck.jsx
+++ src/components/shop/CardLineShopStuck.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+import GenericCardInfosContext from "../../context/genericCardInfosContext";
 
 const CardLineShopStuck = ({ card }) => {
+  //DEFINED langages and Conditions
+  const { lang, conditions } = useContext(GenericCardInfosContext);
+
+  console.log(conditions);
   return (
     <>
       <tr>
         <td>{card.name}</td>
         <td>{card.set}</td>
         <td>/</td>
-        <td>{card.lang}</td>
-        <td>{card.condition}</td>
-        <td>{card.isFoil}</td>
+        <td>{lang.filter(lang => lang.id == card.lang)[0].shortname}</td>
+        <td>
+          {
+            conditions.filter(condition => condition.id == card.condition)[0]
+              .shortname
+          }
+        </td>
+        <td>{card.isFoil == true ? "Yes" : "No"}</td>
         <td>{card.quantity}</td>
         <td>{card.price}</td>
         <td>{card.quantity * card.price}</td>
