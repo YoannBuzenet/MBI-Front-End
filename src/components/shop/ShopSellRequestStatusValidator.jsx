dc53396f7c3292bde916afbd7dd1f7c62e4fb5e2
Yoann <ybuzenet@gmail.com>
2020-02-17 14:44:43 +0100

    structuring Status Validator Component

--- src/components/shop/ShopSellRequestStatusValidator.jsx
+++ src/components/shop/ShopSellRequestStatusValidator.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import AdminSellRequestContext from "../../context/adminSellRequestContext";
 
 const ShopSellRequestStatusValidator = props => {
@@ -6,38 +6,76 @@ const ShopSellRequestStatusValidator = props => {
     AdminSellRequestContext
   );
 
-  //Determine the current status and the actions associated with it
-  var status = "";
-  //Keep this one for submit prop
-  if (currentAdminSellRequest && currentAdminSellRequest.DateSubmit) {
-    status = "Soumis";
-  }
+  const [availableOptions, setAvailableOptions] = useState([]);
 
-  if (currentAdminSellRequest && currentAdminSellRequest.dateEnvoi) {
-    status = "Envoyé";
-  }
-  if (currentAdminSellRequest && currentAdminSellRequest.dateRecu) {
-    status = "Reçu";
-  }
-  if (currentAdminSellRequest && currentAdminSellRequest.dateProcessing) {
-    status = "En traitement";
-  }
-  if (currentAdminSellRequest && currentAdminSellRequest.dateApprovalPending) {
-    status = "En attente de votre validation";
-  }
-  if (currentAdminSellRequest && currentAdminSellRequest.dateValidated) {
-    status = "Validé";
-  }
-  if (currentAdminSellRequest && currentAdminSellRequest.dateCanceled) {
-    status = "Cancelled";
-  }
+  //We initialize the state to Cancelled so that the button to update the Sell Request remain hidden while the real status is being computed
+  const [currentStatus, setCurrentStatus] = useState("Cancelled");
+
+  useEffect(() => {
+    //Keep this one for submit prop
+    if (currentAdminSellRequest && currentAdminSellRequest.DateSubmit) {
+      setCurrentStatus("Soumis");
+    }
+
+    if (currentAdminSellRequest && currentAdminSellRequest.dateEnvoi) {
+      setCurrentStatus("Envoyé");
+    }
+    if (currentAdminSellRequest && currentAdminSellRequest.dateRecu) {
+      setCurrentStatus("Reçu");
+    }
+    if (currentAdminSellRequest && currentAdminSellRequest.dateProcessing) {
+      setCurrentStatus("En traitement");
+    }
+    if (
+      currentAdminSellRequest &&
+      currentAdminSellRequest.dateApprovalPending
+    ) {
+      setCurrentStatus("En attente de votre validation");
+    }
+    if (currentAdminSellRequest && currentAdminSellRequest.dateValidated) {
+      setCurrentStatus("Validé");
+    }
+    if (currentAdminSellRequest && currentAdminSellRequest.dateCanceled) {
+      setCurrentStatus("Cancelled");
+    }
+
+    if (currentStatus == "soumis") {
+      setAvailableOptions([
+        {
+          status: "Reçu",
+          value: "dateRecu"
+        },
+        {
+          status: "En traitement",
+          value: "dateProcessing"
+        },
+        {
+          status: "En attente de validation client",
+          value: "dateApprovalPending"
+        }
+      ]);
+    }
+  });
+
+  const validateSellRequest = () => {
+    console.log("le rachat va etre validé");
+  };
+
+  const cancelSellRequest = () => {
+    console.log("le rachat va etre annulé");
+  };
 
   return (
     <>
       {currentAdminSellRequest.id && (
         <p>Ceci est un menu d'options pour MAJ le statut du rachat</p>
       )}
-      Statut actuel : {status}
+      {currentStatus !== "Validé" && currentStatus !== "Cancelled" && (
+        <>
+          <button onClick={validateSellRequest}>Valider</button>
+          <button onClick={cancelSellRequest}>Annuler</button>
+        </>
+      )}
     </>
   );
 };
