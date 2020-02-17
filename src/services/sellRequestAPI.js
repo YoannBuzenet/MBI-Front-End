325caeafa92098fb9d64346e76af0d361331d669
Yoann <ybuzenet@gmail.com>
2020-02-17 17:04:02 +0100

    context updating is working on Admin Sell Request

--- src/services/sellRequestAPI.js
+++ src/services/sellRequestAPI.js
@@ -18,9 +18,14 @@ function update(id, newData) {
   return axios.put("http://127.0.0.1:8000/sell_requests/" + id, newData);
 }
 
+function updateAsShop(id, newData) {
+  return axios.put("http://127.0.0.1:8000/sell_requestsShop/" + id, newData);
+}
+
 export default {
   send,
   update,
+  updateAsShop,
   findAll,
   findById
 };
