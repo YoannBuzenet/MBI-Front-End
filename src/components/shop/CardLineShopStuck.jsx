b5b8e1c6d9639f8b87cbe6ca4acbdb692f574301
Yoann <ybuzenet@gmail.com>
2020-02-17 20:18:47 +0100

    implemeting stuck cardLine component for validated sell requests

---
+++ src/components/shop/CardLineShopStuck.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const CardLineShopStuck = props => {
+  return (
+    <>
+      <tr>
+        <td>stuck</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </>
+  );
+};
+
+export default CardLineShopStuck;
