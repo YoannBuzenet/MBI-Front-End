0d7f342291b266ca721f1b3fda07e374e8646b20
Yoann <ybuzenet@gmail.com>
2020-02-23 11:16:57 +0100

    preparing parsing prices on One Card Edit

--- src/pages/shopAdmin/ShopAdminOneCard.jsx
+++ src/pages/shopAdmin/ShopAdminOneCard.jsx
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import cardsAPI from "../../services/cardsAPI";
 
 const ShopAdminOneCard = ({ match }) => {
   const { name } = match.params;
 
+  //STATE - All the possibilities
+  const [allPossibleVariations, setAllPossibleVariations] = useState([]);
+
   const nameURLDecoded = decodeURI(name);
-  console.log(nameURLDecoded);
 
   useEffect(() => {
     cardsAPI
       .getByName(nameURLDecoded)
-      .then(data => console.log(data.data["hydra:member"]));
-  });
+      .then(data => {
+        console.log(data.data["hydra:member"]);
+        return data;
+      })
+      .then(data => setAllPossibleVariations(data.data["hydra:member"]));
+
+    //Cancel subscriptions in return
+  }, []);
 
   return (
     <>
-      <h1>One Card setting</h1>
+      <div className="container">
+        <h1>{name}</h1>
+        {allPossibleVariations.map(variation => {
+          return (
+            <div key={variation.id} className="one-set">
+              {variation.name} {variation.edition.name}
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 };
