ec9195523353e20a55676f6156948d101a7a0b4a
Yoann Buzenet <ybuzenet@gmail.com>
2020-02-26 14:53:43 +0100

    baseLang is now placed on top each time for each set

--- src/components/shop/ShopSetLangCards.jsx
+++ src/components/shop/ShopSetLangCards.jsx
@@ -10,26 +10,47 @@ const ShopSetLangCards = ({ variation }) => {
   //TODO : pass this in env variable
   const gradingArea = "isEU";
 
+  //TODO : pass this variable in session + localstorage (its given in resp to POST login)
+  const baseLang = 3;
+
   console.log(lang, conditions);
 
   return (
     <>
       <div className="one-set">
         <h2>{variation.edition.name}</h2>
-        {variation.foreignData
-          .concat([
-            {
-              name: cardsAPI.name,
-              language_id: { id: 9, name: "English", shortname: "EN" }
-            }
-          ])
+        {[
+          {
+            name: cardsAPI.name,
+            language_id: { id: 9, name: "English", shortname: "EN" }
+          }
+        ]
+          .concat(variation.foreignData)
+          .filter(currentLang => currentLang.language_id.id === baseLang)
+          .concat(
+            [
+              {
+                name: cardsAPI.name,
+                language_id: { id: 9, name: "English", shortname: "EN" }
+              }
+            ].filter(currentLang => currentLang.language_id.id !== baseLang)
+          )
+          .concat(
+            variation.foreignData.filter(
+              currentLang => currentLang.language_id.id !== baseLang
+            )
+          )
+
           .map((oneLang, index) => {
             return (
-              <ShopOneLangAllConditionsCard
-                oneLang={oneLang}
-                index={index}
-                key={oneLang.language_id.id}
-              />
+              <>
+                <ShopOneLangAllConditionsCard
+                  oneLang={oneLang}
+                  index={index}
+                  key={oneLang.language_id.id}
+                />
+                <p>Ici les foils</p>
+              </>
             );
           })}
       </div>{" "}
