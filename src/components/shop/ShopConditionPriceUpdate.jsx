50cad7a25f0eadd6b3c62f1d0d702159831e277e
Yoann <ybuzenet@gmail.com>
2020-03-03 14:19:27 +0100

    handle changed implemented, will implement the first version of price update

--- src/components/shop/ShopConditionPriceUpdate.jsx
+++ src/components/shop/ShopConditionPriceUpdate.jsx
@@ -11,10 +11,50 @@ const ShopConditionPriceUpdate = ({
   //TODO : pass this in env variable
   const gradingArea = "nameEU";
 
+  //TODO : pass this in env variable
+  const baseLang = 3;
+
   //DEFINED langages and Conditions
   const { lang, conditions } = useContext(GenericCardInfosContext);
 
   const priceDisplayed = priceValue === null ? "" : priceValue;
+
+  const handlechange = (
+    event,
+    conditionID,
+    langID,
+    isFoil,
+    isInitialized,
+    baseLang
+  ) => {
+    //Checking the input is a number
+    if (!isNaN(parseInt(event.target.value))) {
+      console.log(
+        parseInt(event.target.value),
+        conditionID,
+        langID,
+        isFoil,
+        isInitialized
+      );
+      if (conditionID === 1 && isFoil === 0 && langID === baseLang) {
+        console.log("on met tout à jour pour le NON foil");
+        //update all languages and condition in the current set
+        // send the batch
+      } else if (conditionID === 1) {
+        console.log(
+          "on met toutes les conditions dans la langue donnée et le isFoil donné"
+        );
+        //update all conditions in the given languages
+        //send the batch
+      } else {
+        console.log("on PUT/POST juste celui-là");
+        //send the PUT/POST of this element
+      }
+    } else {
+      //TODO : toast to tell to put a number
+      console.log("type a number please");
+    }
+  };
 
   useEffect(() => {});
 
@@ -24,7 +64,14 @@ const ShopConditionPriceUpdate = ({
         type="text"
         value={priceDisplayed}
         onChange={event => {
-          console.log(event);
+          handlechange(
+            event,
+            conditionID,
+            langID,
+            isFoil,
+            isInitialized,
+            baseLang
+          );
         }}
       />
     </p>
