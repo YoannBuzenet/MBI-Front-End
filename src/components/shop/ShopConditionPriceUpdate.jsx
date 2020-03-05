7bb3b7219eb8088d1f0a2d50c682ec1849509910
Yoann <ybuzenet@gmail.com>
2020-03-05 15:42:52 +0100

    price perf non base Lang are now updated, set in context. Have to send the batch now

--- src/components/shop/ShopConditionPriceUpdate.jsx
+++ src/components/shop/ShopConditionPriceUpdate.jsx
@@ -1,8 +1,8 @@
 import React, { useContext, useEffect, useState } from "react";
 import GenericCardInfosContext from "../../context/genericCardInfosContext";
 import priceBufferContext from "../../context/priceBufferContext";
-import PriceUpdateAPI from "../../services/priceUpdateAPI";
 import priceUpdateAPI from "../../services/priceUpdateAPI";
+import AuthContext from "../../context/authContext";
 
 const ShopConditionPriceUpdate = ({
   conditionID,
@@ -12,6 +12,13 @@ const ShopConditionPriceUpdate = ({
   index,
   cardID
 }) => {
+  //Current Authentication
+  const { authenticationInfos, setAuthenticationInfos } = useContext(
+    AuthContext
+  );
+
+  console.log(authenticationInfos.shop.shopData);
+
   //Context - preparing the DISPLAY context format with data
   const { allPricesBuffer, setAllPricesBuffer } = useContext(
     priceBufferContext
@@ -21,7 +28,6 @@ const ShopConditionPriceUpdate = ({
   const gradingArea = "nameEU";
 
   //TODO : pass this in env variable
-  const baseLang = 3;
   const shop = 3;
 
   //DEFINED langages and Conditions
@@ -38,15 +44,7 @@ const ShopConditionPriceUpdate = ({
       isFoil + "idCardShopPrice"
     ];
 
-  const handlechange = (
-    event,
-    conditionID,
-    langID,
-    isFoil,
-    baseLang,
-    index,
-    cardID
-  ) => {
+  const handlechange = (event, conditionID, langID, isFoil, index, cardID) => {
     //Checking the input is a number
     // console.log(event.target.value);
     if (!isNaN(parseInt(event.target.value))) {
@@ -61,14 +59,69 @@ const ShopConditionPriceUpdate = ({
 
       const newPrice = parseInt(event.target.value);
 
-      if (conditionID === 1 && isFoil === 0 && langID === baseLang) {
+      if (
+        conditionID === 1 &&
+        isFoil === 0 &&
+        langID === authenticationInfos.shop.shopData.baseLang
+      ) {
         console.log("Updating everything but foil cards");
         //update all languages and condition in the current set
         // send the batch
       } else if (conditionID === 1) {
-        console.log("Updating all conditions in given language and isFoilKey");
-        //update all conditions in the given languages
-        //send the batch
+        if (isFoil === 0) {
+          console.log(
+            "Updating all conditions in given language and isFoilKey"
+          );
+          //update all conditions in the given languages
+          const allPricesCopy = [...allPricesBuffer];
+          allPricesCopy[index].langs[langID][1][isFoil] = newPrice;
+
+          //A bit dirty
+          //Browing each condition and setting the price following the percent stored in session.
+          //As session's PercentPerConditions numbers are IN ARRAY, we are hacky and follow their index with -1. The day this order changes, this loop won't work. Solution : receinving data as object instead of array.
+          var i = 1;
+          for (const condition in allPricesCopy[index].langs[langID]) {
+            if (i === 1) {
+              allPricesCopy[index].langs[langID][i][isFoil] = newPrice;
+            } else {
+              allPricesCopy[index].langs[langID][i][isFoil] =
+                //If we want to make prices more stable integer, implement function here
+                (newPrice *
+                  authenticationInfos.shop.shopData.PercentPerConditions[i - 1]
+                    .percent) /
+                100;
+            }
+            i++;
+          }
+
+          setAllPricesBuffer(allPricesCopy);
+
+          //send the batch
+        } else if (isFoil === 1) {
+          //update all conditions in the given languages
+          const allPricesCopy = [...allPricesBuffer];
+          allPricesCopy[index].langs[langID][1][isFoil] = newPrice;
+          //A bit dirty
+          //Browing each condition and setting the price following the percent stored in session.
+          //As session's PercentPerConditions numbers are IN ARRAY, we are hacky and follow their index with -1. The day this order changes, this loop won't work. Solution : receinving data as object instead of array.
+          var i = 1;
+          for (const condition in allPricesCopy[index].langs[langID]) {
+            if (i === 1) {
+              allPricesCopy[index].langs[langID][i][isFoil] = newPrice;
+            } else {
+              allPricesCopy[index].langs[langID][i][isFoil] =
+                //If we want to make prices more stable integer, implement function here
+                (newPrice *
+                  authenticationInfos.shop.shopData.PercentPerConditionFoils[
+                    i - 1
+                  ].percent) /
+                100;
+            }
+            i++;
+          }
+
+          setAllPricesBuffer(allPricesCopy);
+        }
       } else {
         if (
           allPricesBuffer[index].langs[langID][conditionID][isFoil] !== null
@@ -130,15 +183,7 @@ const ShopConditionPriceUpdate = ({
         type="text"
         value={priceDisplayed}
         onChange={event => {
-          handlechange(
-            event,
-            conditionID,
-            langID,
-            isFoil,
-            baseLang,
-            index,
-            cardID
-          );
+          handlechange(event, conditionID, langID, isFoil, index, cardID);
         }}
       />
     </p>
