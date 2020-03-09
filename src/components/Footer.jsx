1308943c1eca67b092ccd60df29286547a33952e
Yoann <ybuzenet@gmail.com>
2020-03-09 11:50:36 +0100

    customizing footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,22 @@ import React from "react";
 const Footer = props => {
   return (
     <>
-      <footer className="footer">Footer</footer>
+      <footer className="footer">
+        <div className="footer-container container">
+          <div>
+            <p>Conditions Générales de Vente</p>
+          </div>
+          <div>
+            <p>Fantasy Sphere</p>
+            <p className="small-text">12, Avenue Saint Michel</p>
+            <p className="small-text">31400 Toulouse</p>
+          </div>
+          <div>
+            <p>05.90.78.64.56</p>
+            <p>fantasySphere@outlook.fr</p>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
