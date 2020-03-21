8f31f2a81b926430ff74654f39003af9cbbb027b
Yoann Buzenet <ybuzenet@gmail.com>
2020-03-21 04:32:42 +0100

    cards are now loaded on cardPage display

--- src/pages/cardPage.jsx
+++ src/pages/cardPage.jsx
@@ -1,4 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import GenericCardInfosContext from "../context/genericCardInfosContext";
+import cardsAPI from "../services/cardsAPI";
 
 const CardPage = ({ match, history }) => {
   //STATE - current card name
@@ -9,6 +12,12 @@ const CardPage = ({ match, history }) => {
     decodeURI(currentName)
   );
 
+  //STATE - current cards displayed
+  const [allCardsDisplayed, setAllCardsDisplayed] = useState([]);
+
+  //CONTEXT langages and Conditions
+  const { lang, conditions } = useContext(GenericCardInfosContext);
+
   useEffect(() => {
     setCurrentName(match.params.cardName);
   }, [match.params.cardName]);
@@ -17,9 +26,32 @@ const CardPage = ({ match, history }) => {
     setCurrentNameDecoded(currentName);
   }, [currentName]);
 
+  useEffect(() => {
+    if (
+      match.params.cardName !== currentName ||
+      allCardsDisplayed.length === 0
+    ) {
+      //Cancel subscriptions preparation
+      const CancelToken = axios.CancelToken;
+      const source = CancelToken.source();
+
+      cardsAPI
+        .getByName(currentNameDecoded, {
+          cancelToken: source.token
+        })
+        .then(data => {
+          // console.log(data.data["hydra:member"]);
+          return data;
+        })
+        .then(data => setAllCardsDisplayed(data.data["hydra:member"]));
+      return () => source.cancel("");
+    }
+  });
+
   return (
     <>
       <h1>{currentName}</h1>
+      {allCardsDisplayed.map(element => console.log(element))}
       CECI EST UNE PAGE MDRRRRR HAHAHHAHAHH
     </>
   );
