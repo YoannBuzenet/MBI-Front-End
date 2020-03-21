2477a256f282f180ee14d26e8339f680f63eb3b5
Yoann Buzenet <ybuzenet@gmail.com>
2020-03-21 11:43:27 +0100

    working on responsive back-office

--- src/pages/shopAdmin/ShopAdminCustomer.jsx
+++ src/pages/shopAdmin/ShopAdminCustomer.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import StatusCalculator from "../../components/StatusCalculator";
 import LastInformationCalculator from "../../components/LastInformationCalculator";
 import { Link } from "react-router-dom";
+import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
 
 const ShopAdminCustomer = ({ match }) => {
   const { id } = match.params;
@@ -31,7 +32,9 @@ const ShopAdminCustomer = ({ match }) => {
 
   return (
     <>
-      <h1>Customer Page</h1>
+      <h1>
+        {customerData && customerData.nom} {customerData && customerData.prenom}
+      </h1>
       <div className="customer-infos">
         <p>Name : {customerData && customerData.nom}</p>
         <p>Prénom : {customerData && customerData.prenom}</p>
@@ -41,42 +44,46 @@ const ShopAdminCustomer = ({ match }) => {
         <p>Postal Code : {customerData && customerData.postalCode}</p>
         <p>Town : {customerData && customerData.town}</p>
       </div>
-
-      <table className="zebra-table">
-        <thead>
-          <tr>
-            <th>Numéro de Rachat</th>
-            <th>Status</th>
-            <th>Date Dernier Traitement</th>
-            <th>Nombre de cartes</th>
-            <th>Montant</th>
-          </tr>
-        </thead>
-        <tbody>
-          {customerData &&
-            customerData.sellrequest &&
-            customerData.sellrequest.length > 0 &&
-            customerData.sellrequest.map(sellRequest => {
-              return (
-                <tr key={sellRequest.id}>
-                  <td>
-                    <Link to={"/shopadmin/sell_requests/" + sellRequest.id}>
-                      {sellRequest.id}
-                    </Link>
-                  </td>
-                  <td>
-                    <StatusCalculator sellRequest={sellRequest} />
-                  </td>
-                  <td>
-                    <LastInformationCalculator sellRequest={sellRequest} />
-                  </td>
-                  <td>{sellRequest.cardTotalQuantity}</td>
-                  <td>{sellRequest.amount}</td>
-                </tr>
-              );
-            })}
-        </tbody>
-      </table>
+      <div className="container">
+        <Table className="zebra-table">
+          <Thead>
+            <Tr>
+              <Th>Numéro de Rachat</Th>
+              <Th>Status</Th>
+              <Th>Date Dernier Traitement</Th>
+              <Th>Nombre de cartes</Th>
+              <Th>Montant</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {customerData &&
+              customerData.sellrequest &&
+              customerData.sellrequest.length > 0 &&
+              customerData.sellrequest.map(sellRequest => {
+                return (
+                  <Tr
+                    key={sellRequest.id}
+                    className="cursor-pointer"
+                    onClick={() =>
+                      (window.location.href =
+                        "/shopadmin/sell_requests/" + sellRequest.id)
+                    }
+                  >
+                    <Td>{sellRequest.id}</Td>
+                    <Td>
+                      <StatusCalculator sellRequest={sellRequest} />
+                    </Td>
+                    <Td>
+                      <LastInformationCalculator sellRequest={sellRequest} />
+                    </Td>
+                    <Td>{sellRequest.cardTotalQuantity}</Td>
+                    <Td>{sellRequest.amount}</Td>
+                  </Tr>
+                );
+              })}
+          </Tbody>
+        </Table>
+      </div>
     </>
   );
 };
