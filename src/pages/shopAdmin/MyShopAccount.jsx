a567dbb5eefcfe63bd6245bb0d84f559a7218db3
Yoann Buzenet <ybuzenet@gmail.com>
2020-03-25 17:54:58 +0100

    corrected bug on History mising in admin navbar

--- src/pages/shopAdmin/MyShopAccount.jsx
+++ src/pages/shopAdmin/MyShopAccount.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import AuthContext from "../../context/authContext";
 
 const MyShopAccount = props => {
+  //Current Authentication
+  const { authenticationInfos, setAuthenticationInfos } = useContext(
+    AuthContext
+  );
+
+  console.log(authenticationInfos);
+
   return <>Shop Account </>;
 };
 
