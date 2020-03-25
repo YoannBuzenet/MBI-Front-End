c0c4a05bc184a431f4aee77bd2dcba293bde0ee1
Yoann Buzenet <ybuzenet@gmail.com>
2020-03-25 16:23:27 +0100

    admin burger menu is live

--- src/components/shop/BurgerMenuShop.jsx
+++ src/components/shop/BurgerMenuShop.jsx
@@ -41,7 +41,79 @@ const BurgerMenuShop = ({ history }) => {
 
   return (
     <>
-      <div className="responsive_menu">Burger menu shop</div>{" "}
+      <div className="responsive_menu responsive_menu_admin">
+        <h2 className="surname_responsive">
+          {authenticationInfos.customer.prenom}
+        </h2>
+        <span className="divider">
+          <hr></hr>
+        </span>
+        <div className="">
+          <ul className="">
+            <Link
+              className="classic_links_responsive classic_links_responsive_admin"
+              to="/my_selling_basket"
+            >
+              <li>
+                Mon Rachat (
+                <span className="buying-total">
+                  {currentBasket.reduce((total, card) => {
+                    return total + card.quantity;
+                  }, 0)}
+                </span>
+                )
+              </li>
+            </Link>
+
+            <Link
+              className="classic_links_responsive classic_links_responsive_admin nav-element"
+              to="/shopadmin/cards"
+            >
+              <li>Cartes</li>
+            </Link>
+            <Link
+              className="classic_links_responsive classic_links_responsive_admin nav-element"
+              to="/shopadmin/sell_requests"
+            >
+              <li>Rachats</li>
+            </Link>
+            <Link
+              className="classic_links_responsive classic_links_responsive_admin nav-element"
+              to="/shopadmin/customers"
+            >
+              <li>Clients</li>
+            </Link>
+
+            <Link
+              to="/my_account"
+              className="classic_links_responsive classic_links_responsive_admin"
+            >
+              <li>Mon compte</li>
+            </Link>
+
+            <Link
+              to="/shopadmin/shopInfos"
+              className="classic_links_responsive classic_links_responsive_admin nav-element"
+            >
+              <li>Informations Boutique</li>
+            </Link>
+
+            <Link
+              to="/shopadmin/settings"
+              className="classic_links_responsive classic_links_responsive_admin nav-element"
+            >
+              <li>Paramètres Gestion</li>
+            </Link>
+
+            <li
+              onClick={handleLogout}
+              className="classic_links_responsive classic_links_responsive_admin"
+            >
+              Déconnexion
+            </li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 };
