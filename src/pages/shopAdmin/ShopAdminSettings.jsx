ac7e189417605558083add8487fe81e4fb2030cf
Yoann Buzenet <ybuzenet@gmail.com>
2020-03-25 19:51:24 +0100

    implementing PercentPerLang form

--- src/pages/shopAdmin/ShopAdminSettings.jsx
+++ src/pages/shopAdmin/ShopAdminSettings.jsx
@@ -23,6 +23,46 @@ const ShopAdminSettings = props => {
           aux autres langues ? tableau avec toutes les autres langues et un
           nombre à rentrer entre 0 et 100%
         </p>
+        <div className="percentPerLang">
+          <form>
+            <label htmlFor="englishPercent">Anglais</label>
+            <input type="text" name="englishPercent" id="englishPercent" />
+            <label htmlFor="frenchPercent">Français</label>
+            <input type="text" name="frenchPercent" id="frenchPercent" />
+            <label htmlFor="germanPercent">Allemand</label>
+            <input type="text" name="germanPercent" id="germanPercent" />
+            <label htmlFor="spanishPercent">Espagnol</label>
+            <input type="text" name="spanishPercent" id="spanishPercent" />
+            <label htmlFor="italianPercent">Italien</label>
+            <input type="text" name="italianPercent" id="italianPercent" />
+            <label htmlFor="portuguesePercent">Portuguais</label>
+            <input
+              type="text"
+              name="portuguesePercent"
+              id="portuguesePercent"
+            />
+            <label htmlFor="japanesePercent">Japonais</label>
+            <input type="text" name="japanesePercent" id="japanesePercent" />
+            <label htmlFor="chineseTraditionalPercent">
+              Chinois Traditionnel
+            </label>
+            <input
+              type="text"
+              name="chineseTraditionalPercent"
+              id="chineseTraditionalPercent"
+            />
+            <label htmlFor="chineseSimplifiedPercent">Chinois Simplifié</label>
+            <input
+              type="text"
+              name="chineseSimplifiedPercent"
+              id="chineseSimplifiedPercent"
+            />
+            <label htmlFor="koreanPercent">Coréen</label>
+            <input type="text" name="koreanPercent" id="koreanPercent" />
+            <label htmlFor="russianPercent">Russe</label>
+            <input type="text" name="russianPercent" id="russianPercent" />
+          </form>
+        </div>
       </div>
     </>
   );
