e59a1ed3d8d45b1262cb710103fb3007336cb672
Yoann Buzenet <ybuzenet@gmail.com>
2020-03-25 10:52:29 +0100

    working on CardPage prices display

--- src/services/sellingBasketAPI.js
+++ src/services/sellingBasketAPI.js
@@ -2,9 +2,11 @@ function save(sellBasket) {
   window.localStorage.setItem("userSellingBasket", JSON.stringify(sellBasket));
 }
 
+/*
+ * Get the currentBasket saved in local storage
+ */
 function getSaved() {
   const savedBasket = window.localStorage.getItem("userSellingBasket");
-
   return JSON.parse(savedBasket);
 }
 
