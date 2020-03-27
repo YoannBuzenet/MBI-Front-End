7b18b203cde2a83d493a3e31fce09df9f1c37a85
Yoann Buzenet <ybuzenet@gmail.com>
2020-03-27 09:47:44 +0100

    PercentPer update works

--- src/services/shopAPI.js
+++ src/services/shopAPI.js
@@ -1,7 +1,27 @@
 import axios from "axios";
 
-function updatePercentPer(shopID, object) {
-  return axios.put("http://127.0.0.1:8000/usersShop/" + shopID, object);
+function updatePercentPerLang(PercentPerLangID, object) {
+  return axios.put(
+    "http://127.0.0.1:8000/percent_per_langs/" + PercentPerLangID,
+    object
+  );
+}
+function updatePercentPerCondition(PercentPerConditionID, object) {
+  return axios.put(
+    "http://127.0.0.1:8000/percent_per_conditions/" + PercentPerConditionID,
+    object
+  );
+}
+function updatePercentPerConditionFoil(PercentPerConditionFoilID, object) {
+  return axios.put(
+    "http://127.0.0.1:8000/percent_per_condition_foils/" +
+      PercentPerConditionFoilID,
+    object
+  );
 }
 
-export default { updatePercentPer };
+export default {
+  updatePercentPerLang,
+  updatePercentPerCondition,
+  updatePercentPerConditionFoil
+};
