253700a961f8d365022964f41b1d748e74c70a8b
Yoann Buzenet <ybuzenet@gmail.com>
2020-03-27 15:54:31 +0100

    infoshop are saved in localStorage

--- src/components/shop/ShopConditionPriceUpdate.jsx
+++ src/components/shop/ShopConditionPriceUpdate.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import priceBufferContext from "../../context/priceBufferContext";
 import priceUpdateAPI from "../../services/priceUpdateAPI";
 import AuthContext from "../../context/authContext";
