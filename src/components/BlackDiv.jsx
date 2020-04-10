9eaf904719dd8499eebc718533b9fdad03a3c01d
Yoann Buzenet <ybuzenet@gmail.com>
2020-04-10 11:18:09 +0200

    working on black div to remove anything displayed on it

--- src/components/BlackDiv.jsx
+++ src/components/BlackDiv.jsx
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
+import BlackDivContext from "../context/blackDivModalContext";
+import CardDisplayOnPageContext from "../context/cardDisplayOnPageContext";
+import isResponsiveMenuDisplayedContext from "./context/menuDisplayedContext";
 
-const BlackDiv = props => {
+// The black Div is the modal we display behind each 'absolute positionned' elements on the window.
+// We give it the general ability to deactivate any absolute display by clicking on it.
+
+const BlackDiv = (props) => {
+  //Black Div control
+  const { isBlackDivModalDisplayed, setIsBlackDivModalDisplayed } = useContext(
+    BlackDivContext
+  );
+
+  //Responsive Menu control
+  const { cardDisplayInformation, setCardDisplayInformation } = useContext(
+    CardDisplayOnPageContext
+  );
+
+  //Card Display control
+  const {
+    isResponsiveMenuDisplayed,
+    setIsResponsiveMenuDisplayed,
+  } = useContext(isResponsiveMenuDisplayedContext);
+
+  const handleClick = (event) => {
+    console.log("removing the div and everything on it");
+  };
+
   return <div className="blackDiv"></div>;
 };
 
