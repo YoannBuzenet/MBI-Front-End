1111926378ae72f3fe8263caa29d21a92184886f
Yoann Buzenet <ybuzenet@gmail.com>
2020-04-11 02:18:26 +0200

    working on MKM Modal

--- src/components/MKMConnectModal.jsx
+++ src/components/MKMConnectModal.jsx
@@ -11,36 +11,43 @@ const MKMConnectModal = (props) => {
   return (
     <div className="MKM-connection-modal">
       <div>
-        <h2>Connection à Magic Card Market</h2>
-        <p>
-          <span className="styled-ol-number">1.</span>Identifie toi sur le site
-          MKM
-        </p>
-        <p>
-          <a
-            href={
-              MKMAPI.MKM_AUTHENTICATION_URL_BASE +
-              authenticationInfos.shop.appToken
-            }
-            target="_blank"
-          >
-            &#8594; Clique ici
-          </a>
-        </p>
-        <p>
-          <span className="styled-ol-number">2.</span>Une fois
-          l'authentification réussie, tu seras redirigé sur
-          www.mtgInterface.com. Connecte toi sur ce site.
-        </p>
-        <p>
-          <span className="styled-ol-number">3.</span>Une fois identifié avec le
-          message de validation sur www.mtgInterface.com, tu n'as plus qu'à
-          cliquer sur ce bouton ci-dessous :
-        </p>
-        <p className="syncronization-button">
-          <span className="sync-symbol">&#8634;</span>
-          <span className="button-content">Synchroniser tous les sites</span>
-        </p>
+        <div className="mkm-modal-container">
+          <h2>Connection à Magic Card Market</h2>
+          <div className="MKM-rule">
+            <span className="styled-ol-number">1.</span>
+            <p>Identifie toi sur le site MKM</p>
+          </div>
+          <p className="mkm_link_auth">
+            <a
+              href={
+                MKMAPI.MKM_AUTHENTICATION_URL_BASE +
+                authenticationInfos.shop.appToken
+              }
+              target="_blank"
+            >
+              &#8594; Clique ici
+            </a>
+          </p>
+          <div className="MKM-rule">
+            <span className="styled-ol-number">2.</span>
+            <p>
+              Une fois l'authentification réussie, tu seras redirigé sur
+              www.mtgInterface.com. Connecte toi sur ce site.
+            </p>
+          </div>
+          <div className="MKM-rule">
+            <span className="styled-ol-number">3.</span>
+            <p>
+              Une fois identifié avec le message de validation sur
+              www.mtgInterface.com, tu n'as plus qu'à cliquer sur ce bouton
+              ci-dessous.
+            </p>
+          </div>
+          <p className="syncronization-button">
+            <span className="sync-symbol">&#8634;</span>
+            <span className="button-content">Synchroniser tous les sites</span>
+          </p>
+        </div>
       </div>
     </div>
   );
