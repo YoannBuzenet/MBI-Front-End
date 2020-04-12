b9e6a0d8876eb6bc436431ba737d6246e5df9ab4
Yoann Buzenet <ybuzenet@gmail.com>
2020-04-12 12:39:41 +0200

    array of ForeignData transformed into sorted Object and transfered to child Lang component

--- src/components/SetLangChoice.jsx
+++ src/components/SetLangChoice.jsx
@@ -4,9 +4,10 @@ import languagesDefinition from "../definitions/languagesDefinition";
 //use languagesDefinition.langDefinitionIDShortName
 
 const SetLangChoice = ({ langsAvailable }) => {
+  console.log(langsAvailable);
   return (
     <div className="set-lang-choosing">
-      {langsAvailable.map((lang, index) => console.log(lang))}
+      {Object.keys(langsAvailable).map((lang, index) => console.log(lang))}
     </div>
   );
 };
