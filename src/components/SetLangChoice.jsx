f6a615bfee43a9ae35e204aa07018e335528dbcb
Yoann Buzenet <ybuzenet@gmail.com>
2020-04-12 12:58:01 +0200

    flags displayed following foreignData, CSS to correct display + make it dynamic

--- src/components/SetLangChoice.jsx
+++ src/components/SetLangChoice.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import languagesDefinition from "../definitions/languagesDefinition";
 
-//use languagesDefinition.langDefinitionIDShortName
-
 const SetLangChoice = ({ langsAvailable }) => {
-  console.log(langsAvailable);
   return (
     <div className="set-lang-choosing">
-      {Object.keys(langsAvailable).map((lang, index) => console.log(lang))}
+      {Object.keys(langsAvailable).map((lang, index) => (
+        <div className="flag-drop-down" key={lang}>
+          <img
+            src={
+              "/flags/25X13/" +
+              languagesDefinition.langDefinitionIDShortName[lang] +
+              ".png"
+            }
+            alt=""
+          />
+        </div>
+      ))}
     </div>
   );
 };
