c04f8fb58e1a09a4b18cc991ead89bf4f02408de
Yoann Buzenet <ybuzenet@gmail.com>
2020-04-18 19:01:24 +0200

    refactoring App Language Defition in config.js

--- src/components/AppWrapper.jsx
+++ src/components/AppWrapper.jsx
@@ -1,16 +1,14 @@
 import React, { useState } from "react";
 import { IntlProvider } from "react-intl";
 import SelectAppLangContext from "../context/selectedAppLang";
-import English from "../translations/English.json";
-import French from "../translations/French.json";
+import config from "../services/config";
 
 const AppWrapper = (props) => {
   //STATE - App Lang
   //Test that : : fr-FR, en-US, en-GB
-  const [currentLang, setCurrentLang] = useState({
-    locale: "fr-FR",
-    translationsForUsersLocale: English,
-  });
+  const [currentLang, setCurrentLang] = useState(
+    config.websiteDefaultLanguageContext
+  );
 
   //CONTEXT CREATION
   const AppLangContext = {
--- src/services/config.js
+++ src/services/config.js
@@ -1,3 +1,6 @@
+import English from "../translations/English.json";
+import French from "../translations/French.json";
+
 const URL_API = "http://mtgapi.local:8888";
 
 const shopID = 1;
@@ -6,9 +9,7 @@ const avatarShopURL = "";
 
 const shopName = "Fantasy Sphere - Nous rachetons toutes vos cartes";
 
-const baseLang = 3;
-const websiteDefaultLanguage = 3;
-
+//DEFINED LANGUAGE ID
 const langDefinition = {
   1: "German",
   2: "Spanish",
@@ -23,22 +24,27 @@ const langDefinition = {
   11: "Traditional Chinese",
 };
 
+//SHOP GRADING SYSTEM
 //EU or US
 const gradingArea = "EU";
 
+//TIME TO AUTO LOG OUT
 //30 minutes
 const TIME_TO_LOG_OUT = 30 * 60 * 1000;
 
+//JWT TOKEN AUTO RENEW
 //55 minutes
 const TIME_JWT_RENEW = 55 * 60 * 1000;
 
+//Clear setTimeout - Throttling time between autologOut timer refresh
 //4 minutes
-//Throttling time between autologOut timer refresh
 const TIME_THROTTLE = 240000;
 
+//COOKIE - LAST MODIFIED CARDS
 //30 minutes
 const TIME_TO_EXPIRE_LAST_MODIFICATION = 30 * 60 * 1000;
 
+//COOKIE - SET LIST
 //2 days
 const TIME_TO_EXPIRE_SET_LIST = 1000 * 60 * 60 * 24 * 2;
 
@@ -50,12 +56,25 @@ const websiteDefaultLanguagearrayLangAvailables = [
   { locale: "en-US", translationsForUsersLocale: English, picture: "EN" },
 ];
 
+//Default website Language ID
+const websiteDefaultLanguage = 3;
+
+//Default Buying Language ID
+const baseLang = 3;
+
+//Default language on the website
+const websiteDefaultLanguageContext = {
+  locale: "en-US",
+  translationsForUsersLocale: English,
+};
+
 export default {
   URL_API,
   shopID,
   shopName,
   baseLang,
   websiteDefaultLanguage,
+  websiteDefaultLanguageContext,
   langDefinition,
   gradingArea,
   TIME_TO_LOG_OUT,
