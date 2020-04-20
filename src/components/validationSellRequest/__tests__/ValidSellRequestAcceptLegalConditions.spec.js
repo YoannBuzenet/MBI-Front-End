937975258c3f01a9e86c9c152533f2bfb175a2c5
Yoann Buzenet <ybuzenet@gmail.com>
2020-04-20 18:00:31 +0200

    first snapshot implemented

--- src/components/validationSellRequest/__tests__/ValidSellRequestAcceptLegalConditions.spec.js
+++ src/components/validationSellRequest/__tests__/ValidSellRequestAcceptLegalConditions.spec.js
@@ -1,8 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import renderer from "react-test-renderer";
 import ValidSellRequestAcceptLegalConditions from "../ValidSellRequestAcceptLegalConditions";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<ValidSellRequestAcceptLegalConditions />, div);
+});
+
+it("renders its UI correctly", () => {
+  const validationComponent = renderer
+    .create(<ValidSellRequestAcceptLegalConditions />)
+    .toJSON();
+  expect(validationComponent).toMatchSnapshot();
 });
