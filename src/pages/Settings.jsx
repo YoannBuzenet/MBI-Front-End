dea12d01ea1c15180d50ecaf84d943f0f89b3e84
Yoann Buzenet <ybuzenet@gmail.com>
2020-04-20 10:07:16 +0200

    setting choice implemented in logged session

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import { FormattedMessage } from "react-intl";
+import AppLangChoice from "../components/AppLangChoice";
 
 const Settings = (props) => {
   return (
-    <h1>
-      <FormattedMessage id="app.settings.title" defaultMessage={`Settings`} />
-    </h1>
+    <>
+      <h1>
+        <FormattedMessage id="app.settings.title" defaultMessage={`Settings`} />
+      </h1>
+      <div className="container">
+        <div className="settings-language-choice">
+          <p>
+            <FormattedMessage
+              id="app.settings.language"
+              defaultMessage={`Language`}
+            />
+          </p>
+          <div className="language-select">
+            <AppLangChoice />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
