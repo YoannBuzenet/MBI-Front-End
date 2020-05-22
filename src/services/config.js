88444b062bdb550aac6ee5b9671df3b6f16ffc55
Yoann <ybuzenet@gmail.com>
2020-05-22 22:20:02 +0200

    test - passed aws in https

--- src/services/config.js
+++ src/services/config.js
@@ -2,7 +2,7 @@ import English from "../translations/English.json";
 import French from "../translations/French.json";
 
 // const URL_API = "http://mtgapi.local:8888";
-const URL_API = "http://ec2-34-255-122-125.eu-west-1.compute.amazonaws.com";
+const URL_API = "https://ec2-34-255-122-125.eu-west-1.compute.amazonaws.com";
 
 const shopID = 1;
 
