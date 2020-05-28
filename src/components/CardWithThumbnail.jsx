b0f3af8d74a8e4b56ddd4a424b7cd40d23f72f1f
Yoann <ybuzenet@gmail.com>
2020-05-28 14:24:26 +0200

    trying different link in thumbnail component

--- src/components/CardWithThumbnail.jsx
+++ src/components/CardWithThumbnail.jsx
@@ -10,45 +10,47 @@ const CardWithThumbnail = ({ card }) => {
 
   return (
     <>
-      <div
-        className="card"
-        onClick={() => (window.location.href = "/card/" + card.name)}
-        //TEST MKM CONNECTION
-        // onClick={() =>
-        //   axios
-        //     .get("https://api.cardmarket.com/ws/v2.0/account", {
-        //       headers: {
-        //         Authorization: MKMAPI.buildOAuthHeader(
-        //           "GET",
-        //           "https://api.cardmarket.com/ws/v2.0/account"
-        //         ),
-        //       },
-        //     })
-        //     .then((data) => console.log(data))
-        //     .catch((error) => {
-        //       if (error.response) {
-        //         console.log(error.response);
-        //       }
-        //       return console.log(error);
-        //     })
-        // }
-      >
-        <div className="card-picture">
-          <img src={picture} alt="" />
-        </div>
-        <div className="card-infos">
-          <div className="card-title">{card.name}</div>
-          <div className="card-set">
-            {card.edition ? card.edition.name : null}
+      <Link to={"/card/" + card.name}>
+        <div
+          className="card"
+          // onClick={() => (window.location.href = "/card/" + card.name)}
+          //TEST MKM CONNECTION
+          // onClick={() =>
+          //   axios
+          //     .get("https://api.cardmarket.com/ws/v2.0/account", {
+          //       headers: {
+          //         Authorization: MKMAPI.buildOAuthHeader(
+          //           "GET",
+          //           "https://api.cardmarket.com/ws/v2.0/account"
+          //         ),
+          //       },
+          //     })
+          //     .then((data) => console.log(data))
+          //     .catch((error) => {
+          //       if (error.response) {
+          //         console.log(error.response);
+          //       }
+          //       return console.log(error);
+          //     })
+          // }
+        >
+          <div className="card-picture">
+            <img src={picture} alt="" />
           </div>
-          <Link to={"/card/" + card.name}>
-            <FormattedMessage
-              id="app.cardThumbnail.seeCardDetails"
-              defaultMessage={`See More`}
-            />
-          </Link>
+          <div className="card-infos">
+            <div className="card-title">{card.name}</div>
+            <div className="card-set">
+              {card.edition ? card.edition.name : null}
+            </div>
+            <Link to={"/card/" + card.name}>
+              <FormattedMessage
+                id="app.cardThumbnail.seeCardDetails"
+                defaultMessage={`See More`}
+              />
+            </Link>
+          </div>
         </div>
-      </div>
+      </Link>
     </>
   );
 };
