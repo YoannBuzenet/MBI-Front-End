b9d8580cad8e51fcc463b4536e74446f2c9eb2ea
Yoann <ybuzenet@gmail.com>
2020-05-28 15:40:46 +0200

    Link correction (problem was coming from amplify)

--- src/components/CardWithThumbnail.jsx
+++ src/components/CardWithThumbnail.jsx
@@ -10,47 +10,45 @@ const CardWithThumbnail = ({ card }) => {
 
   return (
     <>
-      <Link to={"/card/" + card.name}>
-        <div
-          className="card"
-          // onClick={() => (window.location.href = "/card/" + card.name)}
-          //TEST MKM CONNECTION
-          // onClick={() =>
-          //   axios
-          //     .get("https://api.cardmarket.com/ws/v2.0/account", {
-          //       headers: {
-          //         Authorization: MKMAPI.buildOAuthHeader(
-          //           "GET",
-          //           "https://api.cardmarket.com/ws/v2.0/account"
-          //         ),
-          //       },
-          //     })
-          //     .then((data) => console.log(data))
-          //     .catch((error) => {
-          //       if (error.response) {
-          //         console.log(error.response);
-          //       }
-          //       return console.log(error);
-          //     })
-          // }
-        >
-          <div className="card-picture">
-            <img src={picture} alt="" />
-          </div>
-          <div className="card-infos">
-            <div className="card-title">{card.name}</div>
-            <div className="card-set">
-              {card.edition ? card.edition.name : null}
-            </div>
-            <Link to={"/card/" + card.name}>
-              <FormattedMessage
-                id="app.cardThumbnail.seeCardDetails"
-                defaultMessage={`See More`}
-              />
-            </Link>
-          </div>
+      <div
+        className="card"
+        onClick={() => (window.location.href = "/card/" + card.name)}
+        //TEST MKM CONNECTION
+        // onClick={() =>
+        //   axios
+        //     .get("https://api.cardmarket.com/ws/v2.0/account", {
+        //       headers: {
+        //         Authorization: MKMAPI.buildOAuthHeader(
+        //           "GET",
+        //           "https://api.cardmarket.com/ws/v2.0/account"
+        //         ),
+        //       },
+        //     })
+        //     .then((data) => console.log(data))
+        //     .catch((error) => {
+        //       if (error.response) {
+        //         console.log(error.response);
+        //       }
+        //       return console.log(error);
+        //     })
+        // }
+      >
+        <div className="card-picture">
+          <img src={picture} alt="" />
         </div>
-      </Link>
+        <div className="card-infos">
+          <div className="card-title">{card.name}</div>
+          <div className="card-set">
+            {card.edition ? card.edition.name : null}
+          </div>
+          <Link to={"/card/" + card.name}>
+            <FormattedMessage
+              id="app.cardThumbnail.seeCardDetails"
+              defaultMessage={`See More`}
+            />
+          </Link>
+        </div>
+      </div>
     </>
   );
 };
