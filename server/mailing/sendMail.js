4fbb113520cc28d2b09de08c04d77b6c3cd01852
Yoann Buzenet <ybuzenet@gmail.com>
2020-06-12 08:33:22 +0200

    improved switch statement, creating submitted template

--- server/mailing/sendMail.js
+++ server/mailing/sendMail.js
@@ -2,17 +2,43 @@ const ejs = require("ejs");
 const nodemailer = require("nodemailer");
 
 function sendMail(mailRequest) {
-  //TODO : Créer des templates ! Parcourir un Switch pour envoyer le bon template
+  //TODO : Traduire tous les templates
   let template;
   let templateData = {
     user: mailRequest.user,
   };
 
+  //TODO -> On each case, API call must be implemented React side
   switch (mailRequest.action) {
+    case "welcomeEmail":
+      //Unlogged user -> must implement follow up on IP/mail with DB
+      break;
+    case "submitted":
+      //Logged user security
+      //TO DO -> check that the sell request is sent in info prop React Side
+      templateData = { ...templateData, sellRequest: mailRequest.infos };
+      template = __dirname + "/templates/confirmationSellRequestSubmitted.ejs";
+      break;
+    case "cards Sent":
+      //Logged user security
+      break;
+    case "received":
+      //SHOP user security
+      break;
+    case "beingProcessed":
+      //SHOP user security
+      break;
+    case "awaitingCustomerValidation":
+      //SHOP user security
+      break;
+    case "validated":
+      //SHOP user security
+      break;
     case "cancel":
+      //SHOP user security
       //On fait une requete sur l'api centrale en tant que shop avec le jwt. Si on a une 200 alors on envoie le mail de cancel
-      console.log("cancel sell request");
-      console.log(mailRequest.action);
+      //TO DO : CALL TO CHECK USER RIGHTS
+      console.log("action : cancel -> cancel sell request");
       templateData = { ...templateData, sellRequest: mailRequest.infos };
       template = __dirname + "/templates/SellRequestCancellation.ejs";
       break;
