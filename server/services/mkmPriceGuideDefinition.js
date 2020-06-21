2fcfb4c65a06f2c0e24e520684fdf2ac3587ee4b
Yoann <ybuzenet@gmail.com>
2020-06-21 15:43:26 +0200

    added priceguide definition file to prepare selling setting translation Select component

---
+++ server/services/mkmPriceGuideDefinition.js
@@ -0,0 +1,27 @@
+// Key is the MKM id of the priceguide, used on on central API
+// Value is the corresponding translation in /translations/{language}.json
+
+const allPricesAvailableOnMKM = {
+  AvgSellPrice: "app.shop.priceFormUpdate.mkmPriceName.regularMediumPrice",
+  avg1: "app.shop.priceFormUpdate.mkmPriceName.averageRegularSellPriceOnOneDay",
+  avg7:
+    "app.shop.priceFormUpdate.mkmPriceName.averageRegularSellPriceOnSevenDay",
+  avg30:
+    "app.shop.priceFormUpdate.mkmPriceName.averageRegularSellPriceOnThirtyDay",
+  trendPrice: "app.shop.priceFormUpdate.mkmPriceName.averageRegularTrend",
+  germanProLow: "app.shop.priceFormUpdate.mkmPriceName.regularGermanLowPrice",
+  lowPrice: "app.shop.priceFormUpdate.mkmPriceName.regularLowPrice",
+  lowPriceEx: "app.shop.priceFormUpdate.mkmPriceName.regularExcPrice",
+  suggestedPrice: "app.shop.priceFormUpdate.mkmPriceName.suggestedSalePrice",
+  foilSell: "app.shop.priceFormUpdate.mkmPriceName.foilMediumPrice",
+  foilAvg1:
+    "app.shop.priceFormUpdate.mkmPriceName.averageFoilSellPriceOnOneDay",
+  foilAvg7:
+    "app.shop.priceFormUpdate.mkmPriceName.averageFoilSellPriceOnSevenDay",
+  foilAvg30:
+    "app.shop.priceFormUpdate.mkmPriceName.averageFoilSellPriceOnThirtyDay",
+  foilLow: "app.shop.priceFormUpdate.mkmPriceName.foilLowPrice",
+  foilTrend: "app.shop.priceFormUpdate.mkmPriceName.averageFoilTrend",
+};
+
+module.exports = { allPricesAvailableOnMKM };
