544d0205c525fafe59d7f21b40459238c5963b7a
Yoann <ybuzenet@gmail.com>
2020-06-22 20:35:08 +0200

    working on handling change + triggerAPI sending

--- src/components/shop/sellingSettings/SellingSettingsCondition.jsx
+++ src/components/shop/sellingSettings/SellingSettingsCondition.jsx
@@ -1,13 +1,17 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import SellingSettingsContext from "../../../context/sellingSettingsContext";
 import config from "../../../services/config";
 import { allPricesAvailableOnMKM } from "../../../services/mkmPriceGuideDefinition";
-import { useIntl } from "react-intl";
+import { useIntl, FormattedMessage } from "react-intl";
 
 const SellingSettingsCondition = ({ isFoil, condition, lang }) => {
   const { SellingSettings, setSellingSettings } = useContext(
     SellingSettingsContext
   );
+
+  const [timer, setTimer] = useState(null);
+
+  const WAIT_INTERVAL = 2000;
 
   console.log(isFoil, condition, lang);
 
@@ -18,8 +22,41 @@ const SellingSettingsCondition = ({ isFoil, condition, lang }) => {
     config.gradingArea === "EU" ? "shortname" : "shortnameUS";
   const conditionCompleteName = "shortname" + config.gradingArea;
 
-  const handleChange = (event) => {
+  const handleChange = (event, lang, isFoil, condition) => {
+    setTimer(clearTimeout(timer));
     console.log("input");
+    const newContext = SellingSettings;
+    const newPrice = event.target.value;
+    if (!isNaN(parseFloat(event.target.value))) {
+      newContext[lang][condition][isFoil] = newPrice;
+      setTimer(
+        setTimeout(
+          () => triggerAPISending(event, lang, condition, isFoil),
+          WAIT_INTERVAL
+        )
+      );
+    } else if (event.target.value === "") {
+      newContext[lang][condition][isFoil] = 0;
+      setTimer(
+        setTimeout(
+          () => triggerAPISending(event, lang, condition, isFoil),
+          WAIT_INTERVAL
+        )
+      );
+    } else {
+      toast.error(
+        <FormattedMessage
+          id="app.shop.priceFormUpdate.inputNumer.toast.error"
+          defaultMessage={`Please indicate a number.`}
+        />
+      );
+    }
+  };
+
+  const triggerAPISending = (event, lang, condition, isFoil) => {
+    //MAJ l'objet sur Express : json.stringify it and send it to Express
+    //Pas besoin de traitement granulaire comme sur ShopPriceCondition
+    //Juste traiter le fichier et l'envoyer
   };
 
   return (
@@ -35,7 +72,10 @@ const SellingSettingsCondition = ({ isFoil, condition, lang }) => {
           </option>
         ))}
       </select>
-      <input type="text" onChange={handleChange} />
+      <input
+        type="text"
+        onChange={(event) => handleChange(event, lang, isFoil, condition)}
+      />
     </div>
   );
 };
