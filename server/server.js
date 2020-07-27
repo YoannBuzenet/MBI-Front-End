8c4f6e9e3b15475295b01bd6fab19af136b44175
Yoann <ybuzenet@gmail.com>
2020-07-27 22:46:19 +0200

    updating port to remove nginx conflict

--- server/server.js
+++ server/server.js
@@ -163,7 +163,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../build/index.html"));
 });
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 app.listen(port);
 
 console.log("App is listening on port " + port);
