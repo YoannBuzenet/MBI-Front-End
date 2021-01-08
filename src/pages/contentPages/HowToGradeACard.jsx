158b321467ce3a41c3d000e0503015cf9bec7536
Yoann <ybuzenet@gmail.com>
2021-01-09 00:57:29 +0100

    responsive corrected + reworded content

--- src/pages/contentPages/HowToGradeACard.jsx
+++ src/pages/contentPages/HowToGradeACard.jsx
@@ -22,17 +22,9 @@ const HowToGradeACard = () => {
     <div className="container gradingPage">
       <h1>Grader une carte Magic</h1>
       <p>
-        Evaluer correctement l'état d'une carte à Magic est primordial. En
-        effet, cela permet d'apprécier précisément sa valeur, et donc de mettre
-        d'accord les joueurs lors d'une transaction. On parle d'évaluation de
-        l'état d'une carte, ou plus rapidement de "grading" en anglais.
-      </p>
-      <p>
-        Il y a plusieurs catégories d'états de carte, et votre carte rentre
-        forcément dans l'une de ces catégories. Nous avons illustré les
-        catégories avec des images pour aider, mais certaines cartes peuvent
-        rester difficiles à évaluer. Pour cela, il vous faut également lire la
-        description de l'état. Voyons voir !
+        Evaluer correctement l'état d'une carte à Magic est une tâche ardue,
+        mais on s'y fait ! Au cours de ce guide, nous allons essayer de vous
+        montrer comment classer votre carte, selon quelques repères bien précis.
       </p>
       <h2>Mint/MT (Neuve)</h2>
       <div className="cardPicture">
@@ -40,9 +32,12 @@ const HowToGradeACard = () => {
         <img src={MTBack} />
       </div>
       <p>
-        La carte n'a aucune imperfection, et est comme neuve. Habituellement,
-        l'évaluation "Mint" est très peu utilisée entre joueurs, et sert
-        davantage aux collectionneurs pour des cartes plus chères.
+        Quand la carte est Mint, elle n'a aucune imperfection, et est comme
+        neuve. On dit parfois qu'elle est comme "sortie de booster". Les cartes
+        Mint sont très rare car la plus petite imperfection la recale
+        immédiatement au rang de Near-Mint. Habituellement, l'évaluation "Mint"
+        est très peu utilisée entre joueurs, et sert davantage aux
+        collectionneurs pour des cartes rares, et donc... plus chères.
       </p>
       <h2>Near-Mint/NM (Presque neuve)</h2>
       <div className="cardPicture">
@@ -52,8 +47,8 @@ const HowToGradeACard = () => {
       <p>
         Les cartes Near-Mint sont presque neuves. On peut distinguer de rares
         imperfections, mais celles-ci doivent rester très légères. L'ensemble
-        reste dans un état irréprochable. C'est la condition la plus usitée par
-        les joueurs pour les cartes neuves ou quasi-neuves.{" "}
+        reste dans un état irréprochable. C'est la condition la plus utilisée
+        par les joueurs pour les cartes neuves ou quasi-neuves.{" "}
       </p>
       <h2>Excellent/EXC (Légèrement abimée)</h2>
       <div className="cardPicture">
@@ -85,9 +80,10 @@ const HowToGradeACard = () => {
         Les cartes Light Played laissent apparaitre davantage d'usure. Des
         traces blanches très visibles parsèment la carte de part et d'autres.
         Celle-ci a été probablement endommagée en étant manipulée sans
-        pochettes, ou lors d'un rangement périlleux. Les dommages restent
-        toutefois raisonnables : la carte est encore jouable, n'a pas été
-        marquée par une pliure, profondément déformée ou modifiée.
+        pochettes, parfois à même le sol, ou abimée lors d'un rangement
+        périlleux. Les dommages restent toutefois raisonnables : la carte est
+        encore jouable, n'a pas été marquée par une pliure, profondément
+        déformée ou modifiée.
       </p>
       <h2>Played/PL (Serieusement abimée)</h2>
       <div className="cardPicture">
@@ -96,7 +92,7 @@ const HowToGradeACard = () => {
       </div>
       <p>
         Les cartes Played sont très marquées. Les usures et défauts sont
-        présents sur toute la carte.
+        présents sur toute la carte. On voit des marques blanches, des rayures.
       </p>
       <h2>Poor/PO (Très sérieusement abimée)</h2>
       <div className="cardPicture">
@@ -104,12 +100,13 @@ const HowToGradeACard = () => {
         <img src={POBack} />
       </div>
       <p>
-        Les cartes Poor sont très abimées. Il peut y avoir soit beaucoup de
-        petites usures cumulées, soit quelque chose de trop flagrant, qui
+        Les cartes Poor sont encore plus abimées. Pour les cartes Poor, on
+        cherche quelque chose de radical : quelque chose de trop flagrant, qui
         affecte fortement la carte : typiquement une pliure, une marques au
-        feutre, une carte tordue ou fortement gondolée. Les cartes qui sont trop
-        usées pour être jouées en pochettes, c'est à dire encore reconnaissables
-        malgré la pochette, rentrent également dans la catégorie Poor.
+        feutre, une carte tordue ou fortement gondolée, des restes d'eau... Les
+        cartes qui sont trop usées pour être jouées en pochettes, c'est à dire
+        encore reconnaissables malgré la pochette, rentrent également dans la
+        catégorie Poor.
       </p>
       <h3>Informations complémentaires</h3>
       <h2>Signature</h2>
@@ -117,9 +114,12 @@ const HowToGradeACard = () => {
         <img src={cardSigned} />
       </div>
       <p>
-        Une carte signée par son illustrateur doit être indiquée. Certains
-        joueurs adorent cette pratique, d'autres préfèrent l'éviter. Tant que la
-        signature reste discrète, cela n'affecte pas l'état de la carte.
+        Une carte signée par son illustrateur doit être marquée comme telle.
+        Certains joueurs adorent cette pratique, d'autres préfèrent l'éviter.
+        Tant que la signature reste discrète, cela n'affecte pas l'état de la
+        carte. Détail important : une carte signée par une autre personne que
+        l'illustrateur devient une carte feutrée... exception faite si c'est
+        Richard Garfield !
       </p>
       <h2>Altération</h2>
       <div className="cardPicture">
@@ -128,21 +128,16 @@ const HowToGradeACard = () => {
       <p>
         Une carte altérée a été modifiée visuellement. Il existe plusieurs
         degrés d'altération : d'une légère modification à la réinvention totale
-        de la carte. Les cartes qui ont été modifiées graphiquement doivent
-        impérativement être indiquées comme altérées. Bien que ce travail
-        d'artiste soit apprécié de tous, il reste une modification substancielle
-        de la carte et son acquéreur doit être au courant.
+        de la carte.
       </p>
       <h2>Conclusion</h2>
       <p>
-        L'évaluation de l'état d'une carte est un exercice particulier qui
-        devient plus facile avec la pratique. En ce qu'elle est parfois une
-        tâche difficile, nous vous recommandons d'être le plus strict possible
-        lors de l'évaluation de l'état de vos cartes. Si vous hésitez entre deux
-        états, le meilleur moyen de lever un doute est de grader la carte dans
-        l'état le plus abimé, pas principe de prudence. Dans tous les cas, ne
-        vous inquiétez pas, quelqu'un de la boutique vérifiera au cas où. Bonne
-        revente !
+        L'évaluation de l'état d'une carte est un exercice au début un peu
+        complexe, qui devient plus facile avec la pratique. Si vous hésitez
+        entre deux états, le meilleur moyen de lever un doute est de grader la
+        carte dans l'état le plus abimé, pas principe de prudence. Dans tous les
+        cas, ne vous inquiétez pas, quelqu'un de la boutique vérifiera au cas
+        où. Bonne revente !
       </p>
     </div>
   );
