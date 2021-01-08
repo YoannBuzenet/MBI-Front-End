31b049813ed73129e48f34b07fb8119cf6b6d568
Yoann <ybuzenet@gmail.com>
2021-01-09 00:47:02 +0100

    imported pictures, first CSS layer on page

--- src/pages/contentPages/HowToGradeACard.jsx
+++ src/pages/contentPages/HowToGradeACard.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 import { FormattedMessage } from "react-intl";
+import MTFront from "./content/gradingExamples/mintCardFront.png";
+import MTBack from "./content/gradingExamples/mintCardBack.png";
+import NMFront from "./content/gradingExamples/NMFront.png";
+import NMTBack from "./content/gradingExamples/NMBack.png";
+import ExcFront from "./content/gradingExamples/ExcFront.png";
+import ExcBack from "./content/gradingExamples/ExcBack.png";
+import GDFront from "./content/gradingExamples/GDFront.png";
+import GDBack from "./content/gradingExamples/GDBack.png";
+import LPFront from "./content/gradingExamples/LPFront.png";
+import LPBack from "./content/gradingExamples/LPBack.png";
+import PLFront from "./content/gradingExamples/PLFront.png";
+import PLBack from "./content/gradingExamples/PLBack.png";
+import POFront from "./content/gradingExamples/PoorFront.png";
+import POBack from "./content/gradingExamples/PoorBack.png";
+import cardSigned from "./content/gradingExamples/cardSigned.png";
+import cardAltered from "./content/gradingExamples/cardAltered.png";
 
 const HowToGradeACard = () => {
   return (
-    <>
+    <div className="container gradingPage">
+      <h1>Grader une carte Magic</h1>
       <p>
         Evaluer correctement l'état d'une carte à Magic est primordial. En
         effet, cela permet d'apprécier précisément sa valeur, et donc de mettre
@@ -17,38 +34,53 @@ const HowToGradeACard = () => {
         rester difficiles à évaluer. Pour cela, il vous faut également lire la
         description de l'état. Voyons voir !
       </p>
-      <h2>Mint (Neuve)</h2>
-      {/* img 1 // img 2  */}
+      <h2>Mint/MT (Neuve)</h2>
+      <div className="cardPicture">
+        <img src={MTFront} />
+        <img src={MTBack} />
+      </div>
       <p>
         La carte n'a aucune imperfection, et est comme neuve. Habituellement,
         l'évaluation "Mint" est très peu utilisée entre joueurs, et sert
         davantage aux collectionneurs pour des cartes plus chères.
       </p>
-      <h2>Near-Mint (Presque neuve)</h2>
-      {/* img 1 // img 2  */}
+      <h2>Near-Mint/NM (Presque neuve)</h2>
+      <div className="cardPicture">
+        <img src={NMFront} />
+        <img src={NMTBack} />
+      </div>
       <p>
         Les cartes Near-Mint sont presque neuves. On peut distinguer de rares
         imperfections, mais celles-ci doivent rester très légères. L'ensemble
         reste dans un état irréprochable. C'est la condition la plus usitée par
         les joueurs pour les cartes neuves ou quasi-neuves.{" "}
       </p>
-      <h2>Excellent (Légèrement abimée)</h2>
-      {/* img 1 // img 2  */}
+      <h2>Excellent/EXC (Légèrement abimée)</h2>
+      <div className="cardPicture">
+        <img src={ExcFront} />
+        <img src={ExcBack} />
+      </div>
       <p>
         En état Excellent, on commence à discerner quelques imperfections.
         Celles-ci sont visibles, bien que la carte reste en bon état sur
         l'ensemble.
       </p>
-      <h2>Good (Un peu abimée)</h2>
-      {/* img 1 // img 2  */}
+      <h2>Good/GD (Un peu abimée)</h2>
+      <div className="cardPicture">
+        <img src={GDFront} />
+        <img src={GDBack} />
+      </div>
       <p>
         En état Good, les imperfections se multiplient. Des traces blanches
         d'usure sont très distinctement visibles et la carte semble déjà avoir
         un peu vécu. Elle peut contenir des éraflures, ou quelques traces de
         frottement.
       </p>
-      <h2>Light Played (Abimée)</h2>
-      {/* img 1 // img 2  */}
+      <h2>Light Played/LP (Abimée)</h2>
+      <div className="cardPicture">
+        <img src={LPFront} />
+        <img src={LPBack} />
+      </div>
       <p>
         Les cartes Light Played laissent apparaitre davantage d'usure. Des
         traces blanches très visibles parsèment la carte de part et d'autres.
@@ -57,14 +89,20 @@ const HowToGradeACard = () => {
         toutefois raisonnables : la carte est encore jouable, n'a pas été
         marquée par une pliure, profondément déformée ou modifiée.
       </p>
-      <h2>Played (Serieusement abimée)</h2>
-      {/* img 1 // img 2  */}
+      <h2>Played/PL (Serieusement abimée)</h2>
+      <div className="cardPicture">
+        <img src={PLFront} />
+        <img src={PLBack} />
+      </div>
       <p>
         Les cartes Played sont très marquées. Les usures et défauts sont
         présents sur toute la carte.
       </p>
-      <h2>Poor (Très sérieusement abimée)</h2>
-      {/* img 1 // img 2  */}
+      <h2>Poor/PO (Très sérieusement abimée)</h2>
+      <div className="cardPicture">
+        <img src={POFront} />
+        <img src={POBack} />
+      </div>
       <p>
         Les cartes Poor sont très abimées. Il peut y avoir soit beaucoup de
         petites usures cumulées, soit quelque chose de trop flagrant, qui
@@ -75,14 +113,18 @@ const HowToGradeACard = () => {
       </p>
       <h3>Informations complémentaires</h3>
       <h2>Signature</h2>
-      {/* img 1 // img 2  */}
+      <div className="cardPicture">
+        <img src={cardSigned} />
+      </div>
       <p>
         Une carte signée par son illustrateur doit être indiquée. Certains
         joueurs adorent cette pratique, d'autres préfèrent l'éviter. Tant que la
         signature reste discrète, cela n'affecte pas l'état de la carte.
       </p>
       <h2>Altération</h2>
-      {/* img 1 // img 2  */}
+      <div className="cardPicture">
+        <img src={cardAltered} />
+      </div>
       <p>
         Une carte altérée a été modifiée visuellement. Il existe plusieurs
         degrés d'altération : d'une légère modification à la réinvention totale
@@ -102,7 +144,7 @@ const HowToGradeACard = () => {
         vous inquiétez pas, quelqu'un de la boutique vérifiera au cas où. Bonne
         revente !
       </p>
-    </>
+    </div>
   );
 };
 
